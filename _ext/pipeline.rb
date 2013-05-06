d0dff95565c5fbc48450aa47e554cd16e61352b7
LightGuard <lightguard.jp@gmail.com>
2013-05-06 15:39:39 -0600

    Adding the new XML files directories

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -3,5 +3,8 @@ require 'downloads'
 Awestruct::Extensions::Pipeline.new do
   # load up yaml files for downloads
   extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_downloads', 'downloads.xsd')
+  extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_specs', 'specs.xsd')
+  extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_projects', 'projects.xsd')
+  extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_products', 'products.xsd')
 end
 
