8737bcfb515ddad25964ef81d2950160bb1a6634
LightGuard <lightguard.jp@gmail.com>
2013-05-06 15:36:44 -0600

    Changing the name of the extension

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -2,6 +2,6 @@ require 'downloads'
 
 Awestruct::Extensions::Pipeline.new do
   # load up yaml files for downloads
-  extension JBoss::Developer::Awestruct::Extensions::LoadXmlFile.new('_downloads', 'downloads.xsd')
+  extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_downloads', 'downloads.xsd')
 end
 
