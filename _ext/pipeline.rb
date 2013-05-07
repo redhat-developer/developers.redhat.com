ffbfc1febea721d0e5d61b72b1fc61e04f29042c
LightGuard <lightguard.jp@gmail.com>
2013-05-07 13:37:29 -0600

    Adding a new extension to handle XML
    This extension allows for callbacks, similar to a SAX parser.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,4 +1,5 @@
-require 'downloads'
+require 'load_xml'
+require 'read_xml'
 
 Awestruct::Extensions::Pipeline.new do
   # load up yaml files for downloads
@@ -6,5 +7,6 @@ Awestruct::Extensions::Pipeline.new do
   extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_specs', 'specs.xsd')
   extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_projects', 'projects.xsd')
   extension JBoss::Developer::Awestruct::Extensions::LoadXmlFiles.new('_products', 'products.xsd')
+  extension JBoss::Developer::Awestruct::Extensions::ReadXmlFiles.new('_developer-materials', 'developer_materials.xsd')
 end
 
