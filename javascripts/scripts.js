67137faf3a30fd9869f4b0dd97ab779d16f2b1e9
wesbos <wesbos@gmail.com>
2013-08-16 17:33:01 -0400

    layout inital site

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,3 +1,30 @@
-$(function() {
+var app = window.app = {};
+
+/* 
+  Website Cache
+*/
+app.cache = {};
+
+/* 
+  Website Init 
+*/ 
+app.init = function() {
+  
   $(document).foundation();
+
+  /*
+    initialize slider
+  */
+  app.slider = Swipe(document.getElementById('slider'));
+  
+  $('.slider-controls').on('click','a',function(e){
+    var direction = $(this).data('direction');
+    app.slider[direction]();
+    e.preventDefault();
+  })
+
+};
+
+$(function() {
+  app.init();
 });
