ed07df6e29d94fea802d00007d059b5d16528ea1
wesbos <wesbos@gmail.com>
2013-10-15 13:14:16 -0400

    Checking fo SSO session, adding name and image if they are signed in

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -201,6 +201,41 @@ app.createSlider = function($el) {
   return slider;
 };
 
+app.sso = function() {
+  var jbssoserverbase = (document.location.href.indexOf("-stg.") != -1) ? "https://sso-stg.jboss.org" : "https://sso.jboss.org";
+
+  // you can uncomment and fill next variable with another URL to be used for return from SSO login.
+  // Full URL of current page is used normally. 
+  var _jbssobackurl = window.location.href;
+  // you can uncomment and fill next variable with another URL to be used for logout link.
+  // Global SSO logout URL is used normally if not defined. 
+  var _jbssologouturl = window.location.origin + '/login';
+  // postfix appended to returned info snippets before they are placed into HTML 
+  var _jbssoinfopostfix = ' |';
+  
+  // Loads this..
+  $.ajax({
+    // https://sso.jboss.org/logininfo
+    url : jbssoserverbase + "/logininfo?backurl=" + escape(_jbssobackurl) +"&lourl="+ escape(_jbssologouturl),
+    context : document.body,
+    dataType : "jsonp",
+    type : "GET",
+    success : function(data, textStatus) {
+      
+      if (data && data.session) {
+        // user is logged in!
+        var response = $(data.part1),
+            img = response.find('img'),
+            name = response[2].innerText;
+        $('a.logged-in-name').text(name).prepend(img).show();
+        $('[data-dropdown="login"]').hide();
+
+      }
+    }
+  });
+}
+
 $(function() {
   app.init();
+  app.sso();
 });
