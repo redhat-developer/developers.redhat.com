b48bcd102c1afa0e5d92b5d85329500fccd5e4c3
Pete Muir <pmuir@bleepbleep.org.uk>
2013-11-06 12:18:14 +0000

    Trim titles from sections

--- _ext/sections.rb
+++ _ext/sections.rb
@@ -17,9 +17,11 @@ module Awestruct
         site.pages.each do |page|
           if page.content_syntax =~ /^a(sc)?(ii)?(d(oc)?)?$/
             sections = Asciidoctor.load(page.raw_content).sections
-            Hash[sections.map {|s| [s.id, s.render]}.flatten.each_slice(2).to_a].each_pair do |key, value|
-              page.send "#{key}=", value
-            end 
+            sections.each do |s|
+              r = String.new 
+              s.blocks.each {|b| r << b.render}
+              page.send "#{s.id}=", r
+            end
           end
         end
       end
