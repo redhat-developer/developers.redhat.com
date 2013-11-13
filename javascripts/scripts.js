d0bd301017647b6a0bf1e4a8db857c040a6c3fac
wesbos <wesbos@gmail.com>
2013-11-13 13:58:17 -0500

    temp fix for me not being on the VPN

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -282,6 +282,8 @@ app.createSlider = function($el) {
 
 app.sso = function() {
   var jbssoserverbase = (document.location.href.indexOf("-stg.") != -1) ? "https://sso-stg.jboss.org" : "https://sso.jboss.org";
+  // Temporarily set to sso while the not on the vpn
+  jbssoserverbase = "https://sso.jboss.org";
 
   // you can uncomment and fill next variable with another URL to be used for return from SSO login.
   // Full URL of current page is used normally. 
