8581f0e97b6da430c5386da74a59fa5c80a7bb4b
LightGuard <lightguard.jp@gmail.com>
2013-12-09 19:01:43 -0700

    Adding in eap quickstarts
    Still need to send this to the DCP, but they are there. There are also a
    few other small things that need to be fixed up or added in, but at
    least you can see them now.

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,10 +1,13 @@
 require 'zurb-foundation'
 require 'sections'
 require 'common_dir.rb'
+require 'aweplug/extensions/kramdown_quickstart'
 
 Awestruct::Extensions::Pipeline.new do
   # parse AsciiDoc documents and create page variables out of their sections
   extension Awestruct::Extensions::Sections.new
+
+  extension Aweplug::Extensions::Kramdown::Quickstart.new '_eap-quickstarts', 'get-started-item','/quickstarts'
 
   # Load indexifier
   extension Awestruct::Extensions::Indexifier.new
