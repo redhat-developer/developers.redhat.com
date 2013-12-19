07f2f51e22209e347b1352496999263262e1f991
LightGuard <lightguard.jp@gmail.com>
2013-12-19 14:25:58 -0700

    Archetypes now work, push to DCP should work
    Updates for archetypes, boms, possibly some quickstarts from aweplug.
    Information about pushing to the DCP in the readme.
    Updating to newer aweplug.
    Small tweaks on layout.

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ If you are working on the CSS, make sure to also run `compass watch`
 
 New pages should be added to the root with the extension `.html.slim`
 
+### Updating the DCP
+
+In order to update content in the DCP you must have two environment variables set and the URL set in config.yaml.
+The two variables that must be set are `dcp_user` and `dcp_password`. If you need those please contact Pete Muir
+or Jason Porter.
+
+If these two variables are not set you will see a warning during the build:
+
+    Missing username and / or password for searchisko
+
 ## Deployment
 
 Run `rake deploy[staging]` or `rake deploy[production]`
--- _ext/stacks.rb
+++ _ext/stacks.rb
@@ -9,7 +9,7 @@ require 'awestruct/handler_chain'
 require 'pry' # debugging
 
 module JBoss::Developer::Extensions
-  class Stacks
+  class Stacks 
     include Aweplug::Helper::Git::Commit::Metadata
 
     attr_reader :yml
@@ -22,19 +22,18 @@ module JBoss::Developer::Extensions
     )
 
     def initialize stacks, layout, repo
-      @yml = YAML.load_file File.join(repo, stacks)
+      @yml = YAML.load_file File.join(repo, stacks) 
       @layout = layout
-      @commits = commit_info repo, Pathname.new(File.join repo, stacks)
+      @commits = commit_info repo, Pathname.new(File.join repo, stacks) 
     end
 
     def execute site
+      searchisko = Aweplug::Helpers::Searchisko.new({:base_url => site.dcp_base_url,
+                                                     :authenticate => true,
+                                                     :searchisko_username => ENV['dcp_user'],
+                                                     :searchisko_password => ENV['dcp_password'],
+                                                     :logger => site.profile == 'developement'})
       yml['availableRuntimes'].each do |runtime|
-        searchisko = Aweplug::Helpers::Searchisko.new({:base_url => site.dcp_base_url,
-                                                       :authenticate => true,
-                                                       :searchisko_username => ENV['dcp_user'],
-                                                       :searchisko_password => ENV['dcp_password'],
-                                                       #:adapter => :excon,
-                                                       :logger => site.profile == 'developement'})
         # WARNING Hacks below
         case runtime['labels']['runtime-type']
         when 'JPP'
@@ -44,43 +43,153 @@ module JBoss::Developer::Extensions
         else
           next
         end
+
+        (process_boms runtime, site, product, searchisko) unless runtime['archetypes'].nil?
+        (process_archetype runtime, site, product, searchisko) unless runtime['archetypes'].nil?
+      end
+    end
+
+    def process_boms(runtime, site, product, searchisko)
+      bom_page_content = %q!.content[data-slug="#{page.bom['id']}"]
+  p
+    = page.bom['bom']['description']
+  h3 Import code
+  asciidoc:
+    Place the following into your `pom.xml` file of your project.
+
+    [NOTE]
+    ====
+    The suggested version of this BOM is shown by default, but you may
+    choose other versions given your environment.
+    ====
+  h3 Versions
+  .section-auto-sample-vtabs[data-section='vertical-tabs']
+    -for ver in page.bom['allVersions']
+      section class=('active' if ver == page.bom['bom']['recommendedVersion'])
+        p.title
+          a[href='#'] #{ver}
+          .content
+            pre
+              code
+                |
+                  &lt;dependencyManagement>
+                    &lt;dependencies>
+                        &lt;dependency>
+                            &lt;groupId>#{page.bom['bom']['groupId']}&lt;/groupId>
+                            &lt;artifactId>#{page.bom['bom']['artifactId']}&lt;/artifactId>
+                            &lt;version>#{ver}&lt;/version>
+                            &lt;type>pom&lt;/type>
+                            &lt;scope>import&lt;/scope>
+                        &lt;/dependency>
+                    &lt;/dependencies>
+                  &lt;/dependencyManagement>!
+      runtime['boms'].each do |bom|
         bom_page = ::Awestruct::Page.new(site,
                      ::Awestruct::Handlers::LayoutHandler.new(site,
                        ::Awestruct::Handlers::TiltHandler.new(site,
                          ::Aweplug::Handlers::SyntheticHandler.new(site, 'p.test Hello World!',
                                                                    "/products/#{product}/boms.html.slim"))))
         bom_page.layout = @layout
-        bom_page.output_path = "/products/#{product}/boms/index.html"
-        metadata = {:title => 'Bill of Materials'}
-        bom_page.send('metadata=', metadata)
+        bom_page.output_path = "/boms/#{product}/#{bom['bom']['id']}/index.html"
+        metadata = {:title => bom['bom']['name'], :commits => @commits, :boms => []}
         site.pages << bom_page
-        runtime['boms'].each do |bom|
-          bom_dcp = {
-            :sys_type => 'bom',
-            :sys_content_provider => 'jboss-developer',
-            :sys_content_type => 'jboss-developer-bom',
-            :sys_content_id => bom['id'],
-            :sys_updated => @commits.collect {|c| DateTime.parse c[:date]}.first,
-            #:sys_project => '',
-            :sys_contributors => @commits.collect {|c| c[:author]}.uniq,
-            :sys_activity_dates => @commits.collect {|c| DateTime.parse c[:date]},
-            :sys_created => @commits.collect {|c| DateTime.parse c[:date]}.last,
-            :sys_title => bom['bom']['name'],
-            :sys_url_view => "#{site.base_url}#{site.ctx_root.nil? ? '/' : '/' + site.ctx_root + '/'}#{bom_page.output_path}",
-            :sys_description => bom['bom']['description'],
-            #:sys_content => '',
-            #:sys_content_content-type => '',
-            :groupId => bom['bom']['groupId'],
-            :artifactId => bom['bom']['artifactId'],
-            :recommendedVersion => bom['bom']['recommendedVersion'],
-            :versions => yml['availableBomVersions'].select {|b| b['bom']['id'] == bom['bom']['id']}.collect {|b| b['version']}
-          }
-          unless site.profile =~ /development/
-            searchisko.push_content(bom_dcp[:sys_type], bom_dcp[:sys_content_id], bom_dcp.to_json)
-          end
+        bom['allVersions'] = yml['availableBomVersions'].select {|b| b['bom']['id'] == bom['bom']['id']}.collect {|b| b['version']}
+
+        bom_dcp = {
+          :sys_type => 'jbossdeveloper_bom',
+          :sys_content_provider => 'jboss-developer',
+          :sys_content_type => 'bom',
+          :sys_content_id => bom['id'],
+          :sys_updated => @commits.collect {|c| DateTime.parse c[:date]}.first,
+          :sys_contributors => @commits.collect {|c| c[:author]}.uniq,
+          :sys_activity_dates => @commits.collect {|c| DateTime.parse c[:date]},
+          :sys_created => @commits.collect {|c| DateTime.parse c[:date]}.last,
+          :sys_title => bom['bom']['name'],
+          :sys_url_view => "#{site.base_url}#{site.ctx_root.nil? ? '/' : '/' + site.ctx_root + '/'}#{bom_page.output_path}",
+          :sys_description => bom['bom']['description'],
+          :groupId => bom['bom']['groupId'],
+          :artifactId => bom['bom']['artifactId'],
+          :recommendedVersion => bom['bom']['recommendedVersion'],
+          :versions => bom['allVersions']
+        }
+        #metadata[:boms] << bom
+        unless site.profile =~ /development/
+          searchisko.push_content(bom_dcp[:sys_type], bom_dcp[:sys_content_id], bom_dcp.to_json)
         end
+        bom_page.send('metadata=', metadata)
+        bom_page.send('bom=', bom)
       end
     end
 
+    def process_archetype(runtime, site, product, searchisko)
+      archetype_page_content = %q!.content[data-slug="#{page.archetype['id']}"]
+  p
+    = page.archetype['archetype']['description']
+  h3 Import code
+  asciidoc:
+    Place the following into your `pom.xml` file of your project.
+
+    [NOTE]
+    ====
+    The suggested version of this archetype is shown by default, but you may
+    choose other versions given your environment.
+    ====
+  h3 Versions
+  .section-auto-sample-vtabs[data-section='vertical-tabs']
+    -for ver in page.archetype['allVersions']
+      section class=('active' if ver == page.archetype['archetype']['recommendedVersion'])
+        p.title
+          a[href='#'] #{ver}
+          .content
+            pre
+              code
+                |
+                  &lt;dependencyManagement>
+                    &lt;dependencies>
+                        &lt;dependency>
+                            &lt;groupId>#{page.archetype['archetype']['groupId']}&lt;/groupId>
+                            &lt;artifactId>#{page.archetype['archetype']['artifactId']}&lt;/artifactId>
+                            &lt;version>#{ver}&lt;/version>
+                            &lt;type>pom&lt;/type>
+                            &lt;scope>import&lt;/scope>
+                        &lt;/dependency>
+                    &lt;/dependencies>
+                  &lt;/dependencyManagement>!
+      runtime['archetypes'].each do |archetype|
+        archetype_page = ::Awestruct::Page.new(site,
+                           ::Awestruct::Handlers::LayoutHandler.new(site,
+                             ::Awestruct::Handlers::TiltHandler.new(site,
+                               ::Aweplug::Handlers::SyntheticHandler.new(site, archetype_page_content,
+                                                                         "/products/#{product}/boms.html.slim"))))
+        archetype_page.layout = @layout
+        archetype_page.output_path = "/archetypes/#{product}/#{archetype['archetype']['id']}/index.html"
+        metadata = {:title => archetype['archetype']['name'], :commits => @commits, :boms => []}
+        archetype['allVersions'] = yml['availableArchetypeVersions'].select {|b| b['archetype']['id'] == archetype['archetype']['id']}.collect {|b| b['version']}
+
+        archetype_dcp = {
+          :sys_type => 'jbossdeveloper_archetype',
+          :sys_content_provider => 'jboss-developer',
+          :sys_content_type => 'archetype',
+          :sys_content_id => archetype['id'],
+          :sys_updated => @commits.collect {|c| DateTime.parse c[:date]}.first,
+          :sys_contributors => @commits.collect {|c| c[:author]}.uniq,
+          :sys_activity_dates => @commits.collect {|c| DateTime.parse c[:date]},
+          :sys_created => @commits.collect {|c| DateTime.parse c[:date]}.last,
+          :sys_title => archetype['archetype']['name'],
+          :sys_url_view => "#{site.base_url}#{site.ctx_root.nil? ? '/' : '/' + site.ctx_root + '/'}#{archetype_page.output_path}",
+          :sys_description => archetype['archetype']['description'],
+          :groupId => archetype['archetype']['groupId'],
+          :artifactId => archetype['archetype']['artifactId'],
+          :recommendedVersion => archetype['archetype']['recommendedVersion'],
+          :versions => archetype['allVersions']
+        }
+        unless site.profile =~ /development/
+          searchisko.push_content(archetype_dcp[:sys_type], archetype_dcp[:sys_content_id], archetype_dcp.to_json)
+        end
+        archetype_page.send('metadata=', metadata)
+        archetype_page.send('archetype=', archetype)
+        site.pages << archetype_page
+      end
+    end
   end
 end
