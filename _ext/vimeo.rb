9ee3675ce74bcaf9680973b017625692e10402c5
Pete Muir <pmuir@bleepbleep.org.uk>
2014-02-14 11:24:28 +0000

    Allow specifying featured videos.

--- _ext/vimeo.rb
+++ _ext/vimeo.rb
@@ -5,15 +5,14 @@ module JBoss
     module Vimeo
 
       def vimeo(url)
-        id = video_id(url)
-        title = video_title(id)
+        video = Video.new(url, access_token)
         out = "<div class=\"embedded-media\">" +
-        "<h4>#{title}</h4>" +
-        "<iframe src=\"//player.vimeo.com/video/#{id}\?title=0&byline=0&portrait=0&badge=0&color=2664A2\" width=\"500\" height=\"313\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen}></iframe>"
-        cast = video_cast(id)
-        cast.each do |c|
+        "<h4>#{video.title}</h4>" +
+        "<iframe src=\"//player.vimeo.com/video/#{video.id}\?title=0&byline=0&portrait=0&badge=0&color=2664A2\" width=\"500\" height=\"313\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen}></iframe>"
+        video.cast.each do |c|
           out += "<div class=\"follow-links\">" +
           "<span class=\"title\">Follow #{first_name(c.realname)}</span>" +
+          # TODO wire in follow links
           "<a><i class=\"fa fa-rss\"></i></a>" +
           "<a><i class=\"fa fa-facebook\"></i></a>" +
           "<a><i class=\"fa fa-twitter\"></i></a>" +
@@ -23,39 +22,129 @@ module JBoss
         out + "</div>"
       end
 
+      def vimeo_thumb(url)
+        video = Video.new(url, access_token)
+        out = %Q{<a href="#{site.base_url}/video/vimeo/#{video.id}">} +
+        %Q{<img src="#{video.thumb_url}" />} +
+        %Q{</a>} +
+        %Q{<span class="label material-duration">#{video.duration}</span>} +
+        # TODO Add this in once the DCP supports manually adding tags
+        # %Q{<span class="label material-level-beginner">Beginner<span>} +
+        %Q{<h4><a href="#{thumb_url}">#{video.title}</a></h4>} +
+        # TODO Wire in link to profile URL
+        %Q{<p class="author">Author: <a href="#">#{video.author.realname}</a></p>} +
+        %Q{<p class="material-datestamp">Added #{video.upload_date}</p>} +
+        # TODO wire in ratings
+        #%Q{<p class="rating">Video<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-empty"></i><i class="fa fa-star-empty"></i></p>} +
+        %Q{<div class="body"><p>#{video.description}</p></div>}
+        out
+      end
+
       def first_name(full_name)
         full_name.split[0]
       end
 
-      def video_id(url)
-        url.match(/^.*\/(\d*)$/)[1]
-      end
+      class Video
 
-      def video_title(video_id)
-        body = exec_method "vimeo.videos.getInfo", video_id
-        if body
-          JSON.parse(body)["video"][0]["title"]
-        else
-          "Unable to fetch video info from vimeo"
+        def initialize(url, access_token)
+          @id = url.match(/^.*\/(\d*)$/)[1]
+          @access_token = access_token
+          fetch_info
+          fetch_cast
+          fetch_thumb_url
         end
-      end
 
-      def video_cast(video_id)
-        body = exec_method "vimeo.videos.getCast", video_id
-        cast = []
-        if body
-          JSON.parse(body)["cast"]["member"].each do |c|
-            cast << OpenStruct.new(c)
+        def id
+          @id
+        end
+
+        def title
+          @video["title"]
+        end
+
+        def duration
+          t = @video["duration"].to_i
+          Time.at(t).utc.strftime("%T")
+        end
+
+        def upload_date
+          d = @video["upload_date"]
+          DateTime.parse(d).strftime("%F %T")
+        end
+
+        def description
+          d = @video["description"]
+          out = ""
+          if d
+            i = 0
+            max_length = 150
+            d.scan(/[^\.!?]+[\.!?]/).map(&:strip).each do |s|
+              i += s.length
+              if i > max_length
+                break
+              else
+                out += s
+              end
+            end
+          end
+          out
+        end
+
+        def author
+          if @cast[0]
+            @cast[0]
+          else
+            @cast = Openstruct.new({"realname" => "Unknown"})
           end
         end
-        cast
-      end
 
-      def exec_method(method, video_id)
-        if access_token
-          query = "http://vimeo.com/api/rest/v2?method=#{method}&video_id=#{video_id}&format=json"
-          access_token.get(query).body
+        def cast
+          @cast
         end
+
+        def thumb_url
+          @thumb["_content"]
+        end
+
+        def fetch_info
+          body = exec_method "vimeo.videos.getInfo", @id
+          if body
+            @video = JSON.parse(body)["video"][0]
+          else
+            @video = {"title" => "Unable to fetch video info from vimeo"}
+          end
+        end
+
+        def fetch_thumb_url
+          body = exec_method "vimeo.videos.getThumbnailUrls", @id
+          if body
+            @thumb = JSON.parse(body)["thumbnails"]["thumbnail"][1]
+          else
+            @thumb = {"_content" => ""}
+          end
+        end
+
+        def fetch_cast
+          body = exec_method "vimeo.videos.getCast", @id
+          @cast = []
+          if body
+            JSON.parse(body)["cast"]["member"].each do |c|
+              o = OpenStruct.new(c)
+              if o.username != "jbossdeveloper"
+                @cast << o
+              end
+            end
+          end
+        end
+
+
+        def exec_method(method, video_id)
+          if @access_token
+            query = "http://vimeo.com/api/rest/v2?method=#{method}&video_id=#{video_id}&format=json"
+            @access_token.get(query).body
+          end
+        end
+
       end
 
       # Exchange your oauth_token and oauth_token_secret for an AccessToken instance.
