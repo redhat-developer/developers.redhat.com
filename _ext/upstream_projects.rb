c1b46bad92d382fffa3d72d3e1e62b021c47668f
James Cobb <jcobb@redhat.com>
2014-03-11 10:28:30 -0500

    Resolve conflict.
    Issue with my version of hemlock

--- _ext/upstream_projects.rb
+++ _ext/upstream_projects.rb
@@ -1,7 +1,6 @@
 require 'json'
 require 'aweplug/handlers/synthetic_handler'
 require 'aweplug/helpers/searchisko'
-require 'aweplug/cache/yaml_file_cache'
 require 'awestruct/page'
 require 'awestruct/handlers/layout_handler'
 require 'awestruct/handlers/tilt_handler'
@@ -10,12 +9,8 @@ require 'awestruct/handler_chain'
 module JBoss::Developer::Extensions
   class UpstreamProjects
     def execute site
-      if site.cache.nil?
-        site.send('cache=', Aweplug::Cache::YamlFileCache.new)
-      end
       searchisko = Aweplug::Helpers::Searchisko.new({:base_url => site.dcp_base_url,
                                                      :authenticate => false,
-                                                     :cache => site.cache,
                                                      :logger => site.profile == 'developement'})
       page = site.pages.find {|p| p.relative_source_path =~ /projects\.html\.slim/}
 
