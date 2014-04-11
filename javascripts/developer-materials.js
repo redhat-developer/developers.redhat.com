4b99ae78df5c089e4288022650021fb3391d19fe
Vineet Reynolds <Vineet.Reynolds@gmail.com>
2014-04-12 03:10:33 +0530

    DEVELOPER-372 Used ZURB Foundation styles for pagination.
    Pagination is now displayed both at the top and bottom.
    It is hidden when there are no search results.
    When only a single page is available, the next/previous buttons
    are disabled.

--- javascripts/developer-materials.js
+++ javascripts/developer-materials.js
@@ -191,8 +191,8 @@ app.dm = {
     }).done(function(data){
       var hits = data.hits.hits; // first one for testing
       // Create a paginated list
-      $('div.paginator').paging(hits.length, {
-          format: '[< ncnnn >]',
+      $('ul.pagination').paging(hits.length, {
+          format: '< (qq-) nncnn (-pp) >',
           // Display 9 hits in a page
           perpage: 9,
           // When a new page in the list of pages is to be displayed
@@ -201,74 +201,74 @@ app.dm = {
               var data = this.slice;
               for (var i = data[0]; i < data[1]; i++) {
 
-        if ('sys_contributors' in hits[i].fields) {
-            var contributors = hits[i].fields.sys_contributors[0];
-        } else if ('contributors' in hits[i].fields) {
-            var contributors = hits[i].fields.contributors[0];
-        }
+                if ('sys_contributors' in hits[i].fields) {
+                    var contributors = hits[i].fields.sys_contributors[0];
+                } else if ('contributors' in hits[i].fields) {
+                    var contributors = hits[i].fields.contributors[0];
+                }
 
-        var template = "<li class=\"material\">"; 
-        template += "<div class=\"get-started-placeholder-" + hits[i].fields.sys_type + "\" >";
-          if(hits[i].fields.sys_type !== 'video') {
-            template += "<img src='"+app.dm.thumbnails[hits[i].fields.sys_type]+"'>";
-          }
-          if (hits[i].fields.github_repo_url) {
-            var repo = hits[i].fields.github_repo_url;
-            var repoLength = repo.length;
-            if (repo.substring((repoLength - 25), repoLength) === 'jboss-sandbox-quickstarts') {
-              template += "<a class=\"banner experimental\"></a>";
-            }
-          }
-          if (hits[i].fields.sys_url_view) {
-            var url = hits[i].fields.sys_url_view;
-            var premiumPrefix = 'https://access.redhat.com';
-            if (url.substring(0, (premiumPrefix.length)) === premiumPrefix) {
-              template += "<a class=\"banner premium\"></a>";
-            }
-          }
-          if (hits[i].fields.thumbnail) {
-            template += "<a class=\"thumbnail\" href=\"" + hits[i].fields.sys_url_view + "\">";
-            template +="<img onerror=\"this.style.display='none'\" src=\"" + hits[i].fields.thumbnail + "\" />";
-            template += "</a>"
-          }
-          var labels = "";
-          if (hits[i].fields.duration && hits[i].fields.duration > 0) {
-            labels += "<span class=\"material-level-" + hits[i].fields.level + " label\">" +
-              hits[i].fields.duration.toString().toHHMMSS() +
-            "</span>";
-          }
-          if (hits[i].fields.level && hits[i].fields.level.length > 0) {
-            labels += "<span class=\"material-level-" + hits[i].fields.level + " label\">" +
-              hits[i].fields.level +
-            "</span>";
-          }
-          if (labels.length == 0) {
-            labels += "<div class=\"empty-label\"></div>";
-          }
-          template += labels;
-          template += "</div>";
-          template += "<h4>" +
-            "<a href=\"" + hits[i].fields.sys_url_view + "\">" +
-              hits[i].fields.sys_title +
-            "</a>" +
-          "</h4>";
-          if (contributors && contributors.length > 0) {
-           template += "<p class=\"author\">" +
-              "Author:" + 
-              "<a href=\"" + hits[i].fields.sys_url_view + "\">" +
-                contributors +
-              "</a>" +
-            "</p>";
-          }
-          template += "<p class=\"material-datestamp\">" + 
-            "Added " + moment(hits[i].fields.sys_created).fromNow() +
-          "</p>" + 
-          "<div class=\"body\">" +
-            "<p>" +
-              hits[i].fields.sys_description +
-            "</p>" +
-          "</div>" +
-          "</li>";
+                  var template = "<li class=\"material\">"; 
+                  template += "<div class=\"get-started-placeholder-" + hits[i].fields.sys_type + "\" >";
+                  if(hits[i].fields.sys_type !== 'video') {
+                      template += "<img src='"+app.dm.thumbnails[hits[i].fields.sys_type]+"'>";
+                  }
+                  if (hits[i].fields.github_repo_url) {
+                    var repo = hits[i].fields.github_repo_url;
+                    var repoLength = repo.length;
+                    if (repo.substring((repoLength - 25), repoLength) === 'jboss-sandbox-quickstarts') {
+                      template += "<a class=\"banner experimental\"></a>";
+                    }
+                  }
+                  if (hits[i].fields.sys_url_view) {
+                    var url = hits[i].fields.sys_url_view;
+                    var premiumPrefix = 'https://access.redhat.com';
+                    if (url.substring(0, (premiumPrefix.length)) === premiumPrefix) {
+                      template += "<a class=\"banner premium\"></a>";
+                    }
+                  }
+                  if (hits[i].fields.thumbnail) {
+                    template += "<a class=\"thumbnail\" href=\"" + hits[i].fields.sys_url_view + "\">";
+                    template +="<img onerror=\"this.style.display='none'\" src=\"" + hits[i].fields.thumbnail + "\" />";
+                    template += "</a>"
+                  }
+                  var labels = "";
+                  if (hits[i].fields.duration && hits[i].fields.duration > 0) {
+                    labels += "<span class=\"material-level-" + hits[i].fields.level + " label\">" +
+                      hits[i].fields.duration.toString().toHHMMSS() +
+                    "</span>";
+                  }
+                  if (hits[i].fields.level && hits[i].fields.level.length > 0) {
+                    labels += "<span class=\"material-level-" + hits[i].fields.level + " label\">" +
+                      hits[i].fields.level +
+                    "</span>";
+                  }
+                  if (labels.length == 0) {
+                    labels += "<div class=\"empty-label\"></div>";
+                  }
+                  template += labels;
+                  template += "</div>";
+                  template += "<h4>" +
+                    "<a href=\"" + hits[i].fields.sys_url_view + "\">" +
+                      hits[i].fields.sys_title +
+                    "</a>" +
+                  "</h4>";
+                  if (contributors && contributors.length > 0) {
+                   template += "<p class=\"author\">" +
+                      "Author:" +
+                      "<a href=\"" + hits[i].fields.sys_url_view + "\">" +
+                        contributors +
+                      "</a>" +
+                    "</p>";
+                  }
+                  template += "<p class=\"material-datestamp\">" +
+                    "Added " + moment(hits[i].fields.sys_created).fromNow() +
+                  "</p>" +
+                  "<div class=\"body\">" +
+                    "<p>" +
+                      hits[i].fields.sys_description +
+                    "</p>" +
+                  "</div>" +
+                  "</li>";
 
 
                   // Append template to HTML
@@ -287,17 +287,62 @@ app.dm = {
           },
           // Format the paginator
           onFormat: function(type) {
+              if(hits.length < 1) {
+                  return "";
+              }
               switch (type) {
-                  case 'block': // n and c
-                      return '<a>' + this.value + '</a>';
-                  case 'next': // >
-                      return '<a>&gt;</a>';
-                  case 'prev': // <
-                      return '<a>&lt;</a>';
-                  case 'first': // [
-                      return '<a>first</a>';
-                  case 'last': // ]
-                      return '<a>last</a>';
+
+                  case 'block':
+
+                      if (!this.active)
+                          return '<li class="disabled">' + this.value + '</li>';
+                      else if (this.value != this.page)
+                          return '<li><a href="#' + this.value + '">' + this.value + '</a></li>';
+                      return '<li class="current"><a href="#' + this.value + '">' + this.value + '</a></li>';
+
+                  case 'left':
+                  case 'right':
+
+                      if (!this.active) {
+                          return "";
+                      }
+                      return '<li><a href="#' + this.value + '">' + this.value + '</a></li>';
+
+                  case 'next':
+
+                      if (this.active)
+                          return '<li class="next arrow"><a href="#' + this.value + '" class="next">&raquo;</a></li>';
+                      return '<li class="next arrow unavailable"><a href>&raquo;</a></li>';
+
+                  case 'prev':
+
+                      if (this.active)
+                          return '<li class="prev arrow"><a href="#' + this.value + '" class="prev">&laquo;</a></li>';
+                      return '<li class="prev arrow unavailable"><a href>&laquo;</a></li>';
+
+                  case 'first':
+
+                      if (this.active)
+                          return '<li><a href="#' + this.value + '" class="first">First</a></li>';
+                      return '<li class="disabled">First</li>';
+
+                  case 'last':
+
+                      if (this.active)
+                          return '<li><a href="#' + this.value + '" class="last">Last</a></li>';
+                      return '<li class="disabled">Last</li>';
+
+                  case "leap":
+
+                      if (this.active)
+                          return "   ";
+                      return "";
+
+                  case 'fill':
+
+                      if (this.active)
+                          return '<li class="unavailable">...</li>';
+                      return "";
               }
           }
       });
