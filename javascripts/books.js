b972d6ccd89ea7e3b4bcf4212f5f761e02917098
LightGuard <lightguard.jp@gmail.com>
2014-05-07 15:28:56 -0600

    Fixes DEVELOPER-436
    Also shows more books than 10 :) It does look like, however, Google
    isn't finding all of the books we send it. I'm a bit confused by this.

--- javascripts/books.js
+++ javascripts/books.js
@@ -1,48 +1,50 @@
 app.books = {
   getBooks : function(isbnArray) {
-    var query = isbnArray.join(' OR isbn:');
-    query = query.split(' ').join('+');
-    var url = 'https://www.googleapis.com/books/v1/volumes?q='+ encodeURI('isbn:'+query);
-    console.log(url);
-    $.getJSON(url,function(books){
-      var books = window.books = books.items;
-      var bookTemplate = $("#bookTemplate").html();
-      var html = "";
-      console.log(books, bookTemplate);
-      for (var i = 0; i < books.length; i++) {
-        
-        var description = books[i]['volumeInfo']['description'] || "";
-        var shortDescription = description.substring(0,475);
-        if(description.length > 475) {
-          shortDescription+="...";
-        }
+    var html = "";
+    var chunk = 40;
+    for (var i = 0, j = isbnArray.length; i < j; i +=  chunk) {
+      var query = isbnArray.slice(i,i + chunk).join(' OR isbn:');
+      query = query.split(' ').join('+');
+      var url = 'https://www.googleapis.com/books/v1/volumes?q='+ encodeURI('isbn:'+query) + '&maxResults=' + chunk;
+      $.getJSON(url,function(books) {
+        var bookItems = books.items;
+        var bookTemplate = $("#bookTemplate").html();
+        for (var k = 0; k < bookItems.length; k++) {
 
-        var template = bookTemplate.format(
-          books[i]['volumeInfo']['industryIdentifiers'][0]['identifier']       // 0 - isbn
-        , books[i]['volumeInfo']['previewLink']       // 1 - Preview
-        , books[i]['volumeInfo']['imageLinks']['thumbnail']       // 2 - img url
-        , books[i]['volumeInfo']['title']       // 3 - Title
-        , books[i]['volumeInfo']['authors'].join(', ')       // 4 - Author
-        , roundHalf(books[i]['volumeInfo']['averageRating'])       // 5 - Rating
-        , shortDescription       // 6 - description
-        , 'http://www.amazon.com/dp/' +  books[i]['volumeInfo']['industryIdentifiers'][0]['identifier']       // 7 - Purchase Url
-        );
+          var description = bookItems[k]['volumeInfo']['description'] || "";
+          var shortDescription = description.substring(0,475);
+          var thumbnail = ''; // TODO: We need an image made for books that don't have an image
 
-        // Append template to HTML
-        html += template;
-      }
+          if(description.length > 475) {
+            shortDescription+="...";
+          } 
 
-      // Dump HTML into the document
-      $('ul.book-list').html(html);
+          if (bookItems[k]['volumeInfo'].hasOwnProperty('imageLinks')) {
+            thumbnail = bookItems[k]['volumeInfo']['imageLinks']['thumbnail'];
+          }
 
-    });
+          var template = bookTemplate.format(
+            bookItems[k]['volumeInfo']['industryIdentifiers'][0]['identifier']       // 0 - isbn
+            , bookItems[k]['volumeInfo']['previewLink']       // 1 - Preview
+            , thumbnail // 2 - img url
+            , bookItems[k]['volumeInfo']['title']       // 3 - Title
+            , bookItems[k]['volumeInfo']['authors'].join(', ')       // 4 - Author
+            , roundHalf(bookItems[k]['volumeInfo']['averageRating'])       // 5 - Rating
+            , shortDescription       // 6 - description
+            , 'http://www.amazon.com/dp/' +  bookItems[k]['volumeInfo']['industryIdentifiers'][0]['identifier']       // 7 - Purchase Url
+            );
+          // Append template to HTML
+          $('ul.book-list').append(template);
+        } 
+      });
+    }
   }
 };
 
 $(function() {
   // if we are on the books page, pull in the ISBN list
   if($('.isbnList').length) {
-    var isbns = $('.isbnList').text().split(',');
+    var isbns = $('.isbnList').text().trim().split(',');
     app.books.getBooks(isbns);
   }
 });
