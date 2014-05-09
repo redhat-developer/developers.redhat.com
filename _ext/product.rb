ee6c11ca034d2cb5a330fbc76df2274a9b022fbe
LightGuard <lightguard.jp@gmail.com>
2014-05-09 11:46:17 -0600

    Fixes DEVELOPER-470 (forums)

--- _ext/product.rb
+++ _ext/product.rb
@@ -34,7 +34,11 @@ module JBoss
                 # Set the product id to the parent dir
                 product.id = id
                 # Set the forum url to the default value, if not set
-                product.forum_url = site.forums[product.id]['url']
+                if site.forums[product.id]
+                  product.forum_url = site.forums[product.id]['url']
+                else
+                  product.forum_url = ''
+                end
                 if product.current_version
                   # Set the product's current major.minor version
                   product.current_minor_version = product.current_version[/^([0-9]*\.[0-9]*)/, 1]
