36dfe628bba5b7c2b40e41cd723bc4699bb4fe97
LightGuard <lightguard.jp@gmail.com>
2014-05-16 21:25:39 -0600

    Fixes DEVELOPER-482
    Adding some extra JS code to make sure things stay sorted, and also to
    remove duplicates. I'm really not satisfied with Google's Book API, it
    leaves a lot to be desired IMO.

--- javascripts/books.js
+++ javascripts/books.js
@@ -2,6 +2,9 @@
 interpolate: true
 ---
 app.books = {
+  unkownISBN : {
+    'UOM:39076002794183' : '9781933988641'
+  },
   covers : {
     "1782161341" : "book_developingjavaEE6applicationsonjbossas7.jpg",
     "1449345158" : "book_buildingmodularcloudappswithosgi.jpg",
@@ -24,9 +27,13 @@ app.books = {
     var iterations = Math.ceil(isbnArray.length / chunk); // # of ajax requests
     var iterationCount = 0; 
     var currentIteration = 0;
-    var bookData = {};
-    // master book array
-    var bookItems = [];
+    var bookItems = []; // master book array
+    var bookItemsHash = {}; // pseudo map of books, keyed by isbn-13
+
+    // I hate having to iterate multiple times, but I don't see a good way around things
+    for (var i = 0; i < isbnArray.length; i++) {
+      bookItemsHash[isbnArray[i]] = null;
+    }
 
     for (var i = 0, j = isbnArray.length; i < j; i +=  chunk) {
       var query = isbnArray.slice(i,i + chunk).join(' OR isbn:');
@@ -39,14 +46,45 @@ app.books = {
 
         $.getJSON(url,function(books) {
           iterationCount++;
-          bookData[currentIteration] = books.items;
           
+          // Add the book to the hash, we have to try and find the isbn-13 though to keep
+          // keys consistent
+          for (var i = 0; i < books.items.length; i++) {
+            var ids = books.items[i].volumeInfo.industryIdentifiers;
+            if (ids[0].type === "OTHER") {
+              bookItemsHash[app.books.unkownISBN[ids[0].identifier]] = books.items[i];
+              books.items[i].volumeInfo.isbn = ids[0].identifier;
+            } else if (ids[0].type === "ISBN_13") {
+              bookItemsHash[ids[0].identifier] = books.items[i];
+              books.items[i].volumeInfo.isbn = ids[0].identifier;
+            } else if (ids[1].type === "ISBN_13") {
+              bookItemsHash[ids[1].identifier] = books.items[i];
+              books.items[i].volumeInfo.isbn = ids[1].identifier;
+            } else {
+              console.log('can not find ISBN_13');
+              console.log(books.items[i]);
+            }
+          }
+
           // If we have all the data in order, move forward with displaying them
           if(iterations === iterationCount) {
+
             // flatten into one large array
-            for(key in bookData){
-              bookItems = bookItems.concat(bookData[key]);
+            for(key in bookItemsHash){
+              if (bookItemsHash[key]) {
+                bookItems.push(bookItemsHash[key]);
+              }
             } 
+            console.log(bookItems);
+
+            // Make sure all the books are sorted by desc date
+            bookItems.sort(function(a,b) {
+              var date_a = Date.parse(a.volumeInfo.publishDate);    
+              var date_b = Date.parse(b.volumeInfo.publishDate);    
+              if (date_a > date_b) return -1;    
+              if (date_a < date_b) return 1;    
+              return 0;
+            });
             app.books.formatBooks(bookItems);
           }
         });
@@ -62,8 +100,8 @@ app.books = {
 
       var description = bookItems[k]['volumeInfo']['description'] || "";
       var shortDescription = description.substring(0,475);
-      var thumbnail = ''; // TODO: We need an image made for books that don't have an image
-      var isbn = bookItems[k]['volumeInfo']['industryIdentifiers'][0]['identifier'];
+      var thumbnail = ''; 
+      var isbn = bookItems[k]['volumeInfo']['isbn']; 
 
       if(description.length > 475) {
         shortDescription+="...";
@@ -72,7 +110,7 @@ app.books = {
       var template = bookTemplate.format(
         isbn                                                               // 0 - isbn
         , bookItems[k]['volumeInfo']['previewLink']                        // 1 - Preview
-        , app.books.findCover(bookItems[k], isbn)                                          // 2 - img url
+        , app.books.findCover(bookItems[k], isbn)                          // 2 - img url
         , bookItems[k]['volumeInfo']['title']                              // 3 - Title
         , bookItems[k]['volumeInfo']['authors'].join(', ')                 // 4 - Author
         , roundHalf(bookItems[k]['volumeInfo']['averageRating'])           // 5 - Rating
