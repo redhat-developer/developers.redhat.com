2c7f8bdd0ec6928a3df41415a1263b834c5f0d57
Pete Muir <pmuir@bleepbleep.org.uk>
2014-06-03 11:39:17 +0100

    Manually fetch ca-bundle

--- _scripts/jenkins-mac.sh
+++ _scripts/jenkins-mac.sh
@@ -3,12 +3,12 @@
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 if ! type "pngquant" > /dev/null; then
-  # Needs replacing with non-platform specific step!
   brew install pngquant
 fi
 
 if ! type "optipng" > /dev/null; then
-  # Needs replacing with non-platform specific step!
   brew install optipng
 fi
+
+curl http://curl.haxx.se/ca/cacert.pem > cacert.pem
 
