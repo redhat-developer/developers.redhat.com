04f11bdc5ad2b69cb444d2bdd9ea2a5a88b8c532
Pete Muir <pmuir@bleepbleep.org.uk>
2014-06-06 15:35:01 +0100

    Allow commit to fail, if there are no changes

--- _scripts/jenkins-generic.sh
+++ _scripts/jenkins-generic.sh
@@ -28,7 +28,7 @@ rake clean[all] deploy[staging]
 
 ## Commit back CDN changes
 git add _cdn/cdn.yml
-git commit -m "Update cdn.yml"
+git commit -m "Update cdn.yml" || true
 # Fetch any changes made since the build changed
 git pull --rebase origin HEAD
 git push origin HEAD
