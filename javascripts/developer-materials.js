97daa41bfe2c7ebf741c8bd9a39ebb9401043682
wesbos <wesbos@gmail.com>
2014-07-08 11:07:48 -0400

    new select box for developer materials topics DEVELOPER-764
    Updating chosen widget for clear event
    
    Fixing duplicate topics

--- javascripts/developer-materials.js
+++ javascripts/developer-materials.js
@@ -66,7 +66,7 @@ app.dm = {
           case "topics" :
             var valArray = formValue.split(" ");
             $.each(valArray, function(idx, value){
-              $('input[name="filter-topic[]"][value=' + value + ']').attr('checked', true);
+              $('[name="filter-topic[]"][value=' + value + ']').attr('selected', true).attr('checked', true).trigger('change');
             });
             break;
           case "formats":
@@ -118,9 +118,12 @@ app.dm = {
     /*
       Topics
     */ 
-    var topics = $('input[name="filter-topic[]"]:checked').map(function () {
+    var topics = $('[name="filter-topic[]"]:checked').map(function () {
       return this.value;
     }).get(); 
+
+    // remove any duplicates because of having a dropdown and checkbox
+    $.unique(topics);
 
     topics = topics.join(" ");
 
@@ -500,6 +503,8 @@ app.dm = {
         $(el).attr('value',0);
     });
     $('form.dev-mat-filters input:checked').removeAttr('checked');
+    $('form.dev-mat-filters option:selected').removeAttr('selected');
+    $("form.dev-mat-filters select").trigger("chosen:updated");
     $('.filter-rating-active').removeClass('filter-rating-active');
     this.devMatFilter();
   },
@@ -529,7 +534,7 @@ app.dm = {
 // Event Listeners 
 $(function() {
   var timeOut;
-  $('form.dev-mat-filters').on('change keyup','input',function(e){
+  $('form.dev-mat-filters').on('change keyup','input, select',function(e){
     clearTimeout(timeOut);
     timeOut = setTimeout(function() {
       app.dm.devMatFilter();
@@ -574,7 +579,9 @@ $(function() {
     else {
       el.parent().removeClass('checked');
     }
-  });
+  }).trigger('change');
+
+  $('select[name="filter-topic[]"]').chosen();
 
 });
 
