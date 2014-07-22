f9b8a5aae93fddfcc0bfd94e89305e9061fb1fbc
Pete Muir <pmuir@bleepbleep.org.uk>
2014-07-22 11:20:59 +0100

    DEVELOPER-837 install RVM if missing and use RVM in non-interactive mode

--- _ci.sh
+++ _ci.sh
@@ -5,11 +5,11 @@ set -e
 function setup_mac {
   DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-  if ! type "pngquant" > /dev/null; then
+  if ! type "pngquant" &> /dev/null; then
     brew install pngquant
   fi
 
-  if ! type "optipng" > /dev/null; then
+  if ! type "optipng" &> /dev/null; then
     brew install optipng
   fi
 }
@@ -31,21 +31,38 @@ function setup_environment {
   ruby_version=`cat .ruby-version`
   ruby_gemset=`cat .ruby-gemset`
 
-  # Load RVM
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+  # Load or install RVM
+  if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+    export RVM=$HOME/.rvm/bin/rvm
+  elif [ -s "/usr/local/rvm/scripts/rvm" ]; then
+    export RVM="/usr/local/rvm/bin/rvm"
+  else
+    \curl -sSL https://get.rvm.io | bash -s stable --user-install --autolibs=read-only
+    if [ -s "$HOME/.rvm/scripts/rvm" ]; then
+      export RVM=$HOME/.rvm/bin/rvm
+    elif [ -s "/usr/local/rvm/scripts/rvm" ]; then
+      export RVM="/usr/local/rvm/bin/rvm"
+    fi
+  fi
+
+  $RVM autolibs read-only
+
+  $RVM install ${ruby_version}
 
   ## Ruby Setup
   # Ensure we are using the right ruby & gemset
-  rvm_install_on_use_flag=1 rvm --create use ${ruby_version}@${ruby_gemset}
+  $RVM gemset create ${ruby_gemset}
+
+  export RVMDO="$RVM ${ruby_version}@${ruby_gemset} do"
 
   # Make sure rake is installed, so we can execute rake setup
-  if [ $(rvm ${ruby_version}@${ruby_gemset} do gem list rake -i) != "true" ] ; then
-    rvm ${ruby_version}@${ruby_gemset} do gem install rake
+  if [ $($RVMDO gem list rake -i) != "true" ] ; then
+    $RVMDO gem install rake
   fi
 
   ## Build setup
   # Configure the environment. Must run bundle install to bootstrap the env 
-  bundle install
-  rake setup 
+  $RVMDO bundle install
+  $RVMDO rake setup 
 }
 
