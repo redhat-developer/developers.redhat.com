99fafb7435ebc164449baeb650ae9a0628e5a120
LightGuard <lightguard.jp@gmail.com>
2014-11-06 16:42:12 -0700

    Trying to do products in threads

--- _ext/product.rb
+++ _ext/product.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'aweplug/helpers/searchisko'
 require 'aweplug/helpers/video'
 require 'aweplug/cache/file_cache'
+require 'parallel'
 
 module JBoss
   module Developer
@@ -44,7 +45,8 @@ module JBoss
           solutions = []
           site.products = {}
           forum_counts = Hash[JSON.load(@searchisko.search({facet:'per_project_counts', sys_type:'forumthread', size:1}).body)['facets']['per_project_counts']['terms'].map(&:values).map(&:flatten)]
-          site.pages.each do |page|
+          Parallel.each(site.pages, :in_threads => (site.build_threads || 0)) do |page|
+          #site.pages.each do |page|
             if !page.product.nil? && page.relative_source_path.start_with?('/products')
               product = page.product
               id = page.parent_dir
