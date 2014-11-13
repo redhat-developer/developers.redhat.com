jboss-developer/www.jboss.org

DEVELOPER-675 Implement backend to connectors page

_ext/connectors.rb
@@ -10,16 +10,15 @@ module JBoss
       end
 
       def execute site
-        # Run this after the GoogleSpreadsheet extension for connectors
-        searchisko = Aweplug::Helpers::Searchisko.new({:base_url => site.dcp_base_url, 
-                                                       :authenticate => true, 
-                                                       :searchisko_username => ENV['dcp_user'], 
-                                                       :searchisko_password => ENV['dcp_password'], 
-                                                       :cache => site.cache,
-                                                       :logger => site.log_faraday,
-                                                       :searchisko_warnings => site.searchisko_warnings})
+        searchisko = Aweplug::Helpers::Searchisko.default site, 0
+
         Parallel.each(site.fuse_connectors, in_threads: 40) do |connector|
-          searchisko_hash = connector.collect { |(key, value)|
+
+          connectorID = connector[0]
+          connectorData = connector[1]
+
+          searchisko_hash = connectorData.collect { |(key, value)|
+
             case key
             when 'name'
               ['sys_title', value]
@@ -30,11 +29,12 @@ module JBoss
             else
               [key, value]
             end
-          }.merge({'sys_url_view' => "#{site.base_url}/products/fuse/connectors#!id=#{connector['id']}",}).to_h
-          
-          searchisko.push_content('jbossdeveloper_fuse_connectors', 
-                          connector[:id], 
-                          searchisko_hash.to_json)
+          }.to_h.merge({'sys_url_view' => "#{site.base_url}/products/fuse/connectors#!id=#{connectorID}",})
+
+          searchisko.push_content('jbossdeveloper_connector',
+                        connectorID,
+                        searchisko_hash.to_json)
+
         end
       end
 

redhat-developer/developers.redhat.com

_ext/connectors.rb
@@ -10,16 +10,12 @@ module JBoss
       end
 
       def execute site
-        # Run this after the GoogleSpreadsheet extension for connectors
-        searchisko = Aweplug::Helpers::Searchisko.new({:base_url => site.dcp_base_url, 
-                                                       :authenticate => true, 
-                                                       :searchisko_username => ENV['dcp_user'], 
-                                                       :searchisko_password => ENV['dcp_password'], 
-                                                       :cache => site.cache,
-                                                       :logger => site.log_faraday,
-                                                       :searchisko_warnings => site.searchisko_warnings})
-        Parallel.each(site.fuse_connectors, in_threads: 40) do |connector|
-          searchisko_hash = connector.collect { |(key, value)|
+        searchisko = Aweplug::Helpers::Searchisko.default site, 0
+
+        Parallel.each(site.fuse_connectors, in_threads: 5) do |(id, data)|
+
+          searchisko_hash = data.collect { |(key, value)|
+
             case key
             when 'name'
               ['sys_title', value]
@@ -30,11 +26,12 @@ module JBoss
             else
               [key, value]
             end
-          }.merge({'sys_url_view' => "#{site.base_url}/products/fuse/connectors#!id=#{connector['id']}",}).to_h
-          
-          searchisko.push_content('jbossdeveloper_fuse_connectors', 
-                          connector[:id], 
-                          searchisko_hash.to_json)
+          }.to_h.merge({'sys_url_view' => "#{site.base_url}/products/fuse/connectors#!id=#{id}",})
+
+          searchisko.push_content('jbossdeveloper_connector',
+                        id,
+                        searchisko_hash.to_json)
+
         end
       end
 