5b8beaf56efc566be2c5c9bdf43d41394c53f462
ianhamilton <ian.hamilton@rubygemtsl.co.uk>
2015-10-29 13:40:46 +0000

    removed reference to webkit

--- _cucumber/lib/pages/base_page.rb
+++ _cucumber/lib/pages/base_page.rb
@@ -12,7 +12,7 @@ class BasePage < SitePrism::Page
   end
 
   def loaded?(title)
-    if Capybara.current_driver == 'poltergeist'.to_sym || Capybara.current_driver == 'webkit'.to_sym
+    if Capybara.current_driver == 'poltergeist'.to_sym
       raise "There was a problem loading the page, expected status code to be 200, but was #{page.status_code}" unless page.status_code.eql?(200)
     end
     raise "Expected page title to be #{title}, but was #{page.title}" unless page.has_title?(title).eql?(true)
