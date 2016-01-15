508ad6bf1ffcbe7d604f65cda91b48ad34e683a0
ianhamilton <ian.hamilton@rubygemtsl.co.uk>
2016-01-15 14:25:08 +0000

    addressing Olly's comments.

--- _cucumber/features/support/hooks.rb
+++ _cucumber/features/support/hooks.rb
@@ -59,7 +59,7 @@ After do |scenario|
 end
 
 def user_logout
-  if Capybara.app_host == 'http://developers.redhat.com'
+  if Capybara.app_host == 'http://developers.redhat.com/'
     visit("https://developers.redhat.com/auth/realms/rhd/protocol/openid-connect/logout?redirect_uri=#{@redirect_url}%3Fredirect_fragment%3D!")
   else
     visit("https://it-developers.stage.redhat.com/auth/realms/rhd/protocol/openid-connect/logout?redirect_uri=#{@redirect_url}%3Fredirect_fragment%3D!")
