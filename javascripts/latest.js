d9ecc489a1ffc973e9b740c911b078d42e64b458
wesbos <wesbos@gmail.com>
2016-02-10 10:10:57 -0500

    Latest content component on home page

---
+++ javascripts/latest.js
@@ -0,0 +1,43 @@
+/*
+  Latest Component
+  Shows up on the home page
+*/
+
+app = window.app || {};
+
+app.latest = {};
+
+app.latest.fetch = function() {
+  $.getJSON('http://dcp2.jboss.org/v2/rest/search/resources?newFirst=true&size10=true',function(data){
+    if(data.hits && data.hits.hits) {
+      app.latest.render(data.hits.hits);
+    }
+  });
+}
+
+app.latest.render = function(materials) {
+  console.log("Render called");
+  var html = [];
+  materials.forEach(function(material){
+    var type = material.fields.sys_type[0];
+    var timeStamp = new Date(material.fields.sys_created[0]);
+    var timeAgo = $.timeago(timeStamp);
+    var item = [
+      '<li>',
+        '<i class="icon-RHDev_-resources_icons_'+ type +'"></i>',
+        '<a href="' + material.fields.sys_url_view[0] + '" class="title">',
+          material.fields.sys_title[0],
+          '<p class="date">',
+            timeAgo,
+          '</p>',
+        '</a>',
+      '</li>'
+    ].join('');
+
+    html.push(item);
+  });
+
+  $('.homepage-resources-latest').html(html.join(''));
+}
+
+app.latest.fetch();
