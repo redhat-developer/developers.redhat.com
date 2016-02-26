43a9f5cadcbae76de1af2440d956bc2a03c056b2
LightGuard <lightguard.jp@gmail.com>
2016-02-26 16:55:24 +0000

    drupal_host_ip || hostnam  && preview -> gen
    In the drupal profile if there's no ENV['DRUPAL_HOST_IP'] use ENV['HOSTNAME'] instead of just docker as this will mess things up on the developer's box.
    
    Also for drupal we should be doing a gen instead of a preview.

--- _docker/lib/options.rb
+++ _docker/lib/options.rb
@@ -51,7 +51,7 @@ class Options
         tasks[:kill_all] = true
         tasks[:set_ports] = true
         tasks[:supporting_services] += %w(drupal drupalpgsql mysql searchiskoconfigure searchisko)
-        tasks[:awestruct_command_args] = ['--no-deps', '--rm', '--service-ports', 'awestruct', "rake git_setup clean preview[profile]"]
+        tasks[:awestruct_command_args] = ['--no-deps', '--rm', '--service-ports', 'awestruct', "rake git_setup clean gen[drupal]"]
       end
 
       opts.on('-u', '--drupal', 'Start up and enable drupal') do |u|
