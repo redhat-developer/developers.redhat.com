01c6801c505bdc9ca761611f1c75330e4332e59e
LightGuard <lightguard.jp@gmail.com>
2016-03-04 10:59:37 -0700

    Had to rearrange the order of things for a view

--- _docker/drupal/run.sh
+++ _docker/drupal/run.sh
@@ -13,13 +13,13 @@ chown -R root:root sites
 
 drupal site:install standard --db-type=pgsql --db-host=$DRUPALPGSQL_PORT_5432_TCP_ADDR --db-port=$DRUPALPGSQL_PORT_5432_TCP_PORT --db-name=$DB_NAME --db-user=$DB_USER --db-pass=$DB_PASSWORD --account-name=admin --account-mail="admin@example.com" --site-name="Drupal 8 Testing" --site-mail="test@example.com" --account-pass=admin --langcode=en -n
 
-# install the needed modules in drupal, seems to be an issue where we have to install dependencies first, can't do it all at once
+drupal theme:install --set-default rhd
+
+# install the needed modules in drupal, seems to be an issue where we have to install dependencies first, can't do it all at once 
 drupal module:install serialization basic_auth basewidget
 drupal module:install rest layoutmanager
 drupal module:install hal
 drupal module:install redhat_developers
-
-drupal theme:install --set-default rhd
 
 chown -R www-data:www-data sites
 
