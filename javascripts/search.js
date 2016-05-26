7894a9624af90c787e28e1174551d2f42601f576
wesbos <wesbos@gmail.com>
2016-05-26 13:07:14 -0400

    styling and functionality updates
    fixed checkboxes

--- javascripts/search.js
+++ javascripts/search.js
@@ -9,7 +9,7 @@ search.service('searchService',function($http, $q) {
     // fold in params with defaults
     var search = Object.assign(params, {
       // field: '_source',
-      field: ['sys_url_view', 'sys_title', 'sys_last_activity_date', 'sys_description', 'sys_tags','sys_project'],
+      field: ['sys_url_view', 'sys_title', 'sys_last_activity_date', 'sys_description', 'sys_tags','sys_project', 'sys_contributor'],
       agg: ['per_project_counts','tag_cloud', 'top_contributors', 'activity_dates_histogram', 'per_sys_type_counts'],
     });
 
@@ -32,6 +32,7 @@ search.service('searchService',function($http, $q) {
 */
 search.filter('timeAgo', function() {
   return function(timestamp){
+    if(!timestamp) return;
     var date = new Date(timestamp);
     return $.timeago(date);
   }
@@ -41,6 +42,24 @@ search.filter('timestamp', function() {
   return function(timestamp){
     var date = new Date(timestamp);
     return date.getTime();
+  }
+});
+
+search.filter('icon', function() {
+  return function(sys_type){
+    var icons = {
+      video: 'icon-RHDev_-resources_icons_video',
+      blog: 'icon-RHDev_-resources_icons_blogpost',
+      jbossdeveloper_book: 'icon-RHDev_-resources_icons_book',
+      book: 'icon-RHDev_-resources_icons_book',
+      demo: 'icon-RHDev_-resources_icons_demo',
+      quickstart: 'icon-RHDev_-resources_icons_demo',
+      jbossdeveloper_archetype: 'icon-RHDev_-resources_icons_demo',
+      jbossdeveloper_bom: 'icon-RHDev_-resources_icons_demo',
+      quickstart_early_access: 'icon-RHDev_-resources_icons_demo',
+      jbossdeveloper_example: 'icon-RHDev_-resources_icons_getstarted'
+    }
+    return icons[sys_type] || icons.blog;
   }
 });
 
@@ -58,8 +77,8 @@ function searchCtrlFunc($scope, searchService) {
   /* default */
   $scope.params = {
     query: q,
-    sortBy: 'new',
-    size: 5,
+    sortBy: 'score',
+    size: 10,
     from: 0,
     sys_type : [],
     project : ''
@@ -117,11 +136,17 @@ function searchCtrlFunc($scope, searchService) {
     var startAt = (page * $scope.totalCount) - $scope.params.size;
     var endAt = page * $scope.params.size;
     var pages = Math.ceil($scope.totalCount / $scope.params.size);
+    var lastVisible = parseFloat($scope.params.size) + $scope.params.from;
+
+    if($scope.totalCount < lastVisible) {
+      lastVisible = $scope.totalCount;
+    }
 
     $scope.paginate = {
       currentPage: page,
       pagesArray: app.utils.diplayPagination(page, pages, 4),
-      pages: pages
+      pages: pages,
+      lastVisible: lastVisible
     }
   };
 
@@ -156,168 +181,27 @@ function searchCtrlFunc($scope, searchService) {
   };
 
 
-   $scope.toggleSelection = function toggleSelection(event) {
+  $scope.toggleSelection = function toggleSelection(event) {
 
     var checkbox = event.target;
     var topicNames = checkbox.value.split(' ');
 
     if (checkbox.checked) {
-      $scope.params.sys_type = $scope.params.sys_type.concat(topicNames);
+      // Add - allow for multiple checks
+      // $scope.params.sys_type = $scope.params.sys_type.concat(topicNames);
+      // Replace - only allow one thing to be checked
+      $scope.params.sys_type = topicNames;
     }
     else {
-      // TODO: polyfill forEach
       topicNames.forEach(function(topic) {
         var idx = $scope.params.sys_type.indexOf(topic);
         $scope.params.sys_type.splice(idx, 1);
       });
     }
-   };
-
-   /*
-      Date Filter Dropbon
-   */
+    // re run the search
+    $scope.updateSearch();
+  };
 
   $scope.updateSearch(); // run on pageload TODO: move to ng-init
 }
 
-
-
-
-// /* global app */
-
-// app.search = {
-
-//   abort : function() {
-//     // abort previous request if we are running a new one
-//     if(app.search.currentRequest && app.search.currentRequest.readyState !== 4) {
-//       app.search.currentRequest.abort();
-//     }
-//   },
-//   fetch :function(query) {
-
-//     // Pass search params to GTM for analytics
-//     window.dataLayer = window.dataLayer || [];
-//     window.dataLayer.push({ 'keyword' : query });
-//     window.dataLayer.push({'event': 'website-search'});
-
-//     // perform ajax request
-//     $.ajax({
-//       url : app.dcp.url.search,
-//       dataType: 'json',
-//       data : {
-//         "field"  : ["sys_title", "sys_url_view"],
-//         "type" : "jbossdeveloper_website",
-//         "query" : query,
-//         "size" : 10,
-//         "query_highlight" : true
-//       },
-//       beforeSend : function() {
-//         app.search.abort();
-//       },
-//       success : function(data) {
-//         app.search.format(query, data.hits.hits, $('form.search .searchResults'));
-//       },
-//       error : function() {
-//         $('.searchResults').html("<ul>" + app.dcp.error_message + "</ul>");
-//       }
-//     });
-
-//   },
-//   format : function(query, results, container) {
-//     var suggestions = $('<ul>');
-//     for (var i = 0; i < results.length; i++) {
-//       var title = results[i].highlight && results[i].highlight.sys_title ? results[i].highlight.sys_title : results[i].fields.sys_title;
-//       var url = results[i].fields.sys_url_view;
-//       suggestions.append('<li><a href="' + url + '">'+ title  +'</a></li>');
-//     };
-//     // $('.searchResults').html(suggestions);
-//     container.html(suggestions);
-//   }
-
-// };
-
-// // binding
-// (function() {
-//   var timeOut;
-//   $('form.search').on('submit',function(e){
-//     e.preventDefault();
-//   });
-
-//   // listen to key events on the search form and the buzz filter page
-//   $('form.search').on('keyup','input',function(e) {
-//     var form = $(this).parent();
-//     /*
-//       Check for enter / return key
-//     */
-
-//     if(e.keyCode == 13) {
-//       var link = $('.searchResults li.active-item a');
-//       window.location.assign(link[0].href);
-//       // .click();
-//       return;
-//     }
-
-//     /*
-//       Check for up / down arrows
-//     */
-
-//     if(e.keyCode === 38 || e.keyCode === 40) {
-//       if(e.keyCode === 38) { // up arrow
-//         e.preventDefault(); // stop the cursor from going to the front of the input box
-//         var activeItem = form.find('.searchResults li.active-item').prev();
-//       }
-//       else { // down arrow
-//         var activeItem = form.find('.searchResults li.active-item').next();
-//       }
-
-//       // check if there is a selected item in the list
-//       if(!activeItem.length && e.keyCode === 40) { // nothing and down arrow
-//         activeItem = form.find('.searchResults li:first');
-//       }
-
-//       if(!activeItem.length && e.keyCode === 38) { // nothing and up arrow
-//         activeItem = form.find('.searchResults li:last');
-//       }
-
-//       $('.active-item').removeClass('active-item');
-//       activeItem.addClass('active-item');
-//     }
-
-//     /*
-//       Otherwise suggest search results
-//     */
-//     else {
-//       var query = $(this).val();
-//       if(!query) {
-//         $('.searchResults').html('');
-//         return;
-//       }
-//       clearTimeout(timeOut);
-//       timeOut = setTimeout(function() {
-//         // if it is the search form, go ahead and search
-//         // otherwise it is buzz, and we handle this in buzz.js
-//         if(form.hasClass('search')) {
-//           app.search.fetch(query);
-//         }
-//       }, 300);
-//     }
-//   });
-
-//   // When someone hovers over a selection, remove
-//   $('.searchResults').on('mouseover','li',function() {
-//     $('li.active-item').removeClass('active-item');
-//     $(this).addClass('active-item');
-//   });
-
-//   // when someone clicks the search result with their mouse
-//   $('.searchResults').on('mousedown','a',function() {
-//     window.location.assign(this.href);
-//   });
-
-//   // close the search box on mobile
-//   $('.search-close').on('click',function(e){
-//     $('form.search input').val('');
-//     $('.searchResults').html('');
-//   });
-
-// })();
