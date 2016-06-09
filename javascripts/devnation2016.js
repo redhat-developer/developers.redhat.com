3be228228c09739e6169fec0e443a703af77d39f
Paul Robinson <paul.robinson@redhat.com>
2016-06-09 12:06:08 +0100

    WiP: Making local TZ js work for multiple dates

--- javascripts/devnation2016.js
+++ javascripts/devnation2016.js
@@ -1,20 +1,27 @@
 app.devnation2016 = {
   processDate: function () {
-    var eventDate = document.getElementById('hiddenDate');
-    if (eventDate === null)
-      return;
+    var eventDates = document.getElementsByClassName('hiddenDate');
+    for (var i = 0; i < eventDates.length; i++) {
+      
+      if (eventDates[i] === null) {
+        console.log('Found a null');
+        continue;
+      }
 
-    var eventDate = new Date(eventDate.innerHTML);
+      console.log('Parsing:' + eventDates[i].innerHTML);
+      var eventDate = new Date(eventDates[i].innerHTML.toString());
+      console.log('Parsed:' + eventDate);
 
-    // Format and display the date
-    var timezone = (String(String(eventDate).split("(")[1]).split(")")[0]);
-    var d = eventDate.toLocaleDateString();
-    var t = eventDate.toLocaleTimeString();
-    t = t.replace(/\u200E/g, '');
-    t = t.replace(/^([^\d]*\d{1,2}:\d{1,2}):\d{1,2}([^\d]*)$/, '$1$2');
-    $('.event-day').text(d);
-    $('.event-time').text(t);
-    $('.event-timezone').text(timezone);
+      // Format and display the date
+      var timezone = (String(String(eventDate).split("(")[1]).split(")")[0]);
+      var d = eventDate.toLocaleDateString();
+      var t = eventDate.toLocaleTimeString();
+      t = t.replace(/\u200E/g, '');
+      t = t.replace(/^([^\d]*\d{1,2}:\d{1,2}):\d{1,2}([^\d]*)$/, '$1$2');
+      $('.event-day').text(d);
+      $('.event-time').text(t);
+      $('.event-timezone').text(timezone);
+    }
     
   }  
 };
