a1e8fa1f3b0ce1730f10d931f2bee4fde5ff7801
LightGuard <lightguard.jp@gmail.com>
2016-06-09 11:19:12 -0600

    namspace.js no longer needed in Drupal
    See https://issues.jboss.org/browse/DEVELOPER-3144 for more information.
    
    There is a new file drupal-namespace.js which is being used for Drupal,
    in combination with some javascript coming from Drupal based on the
    environment.
    
    I have removed the scripts from the twig files where I had them in place
    to get things working. namespace.js is also no longer being sent with
    pages being rendered by awestruct and input into Drupal. This removed
    the need for the as_is_html format so I removed that as well.
    
    What we're currently using for client side html templates are now stored
    in twig files (but they're not being rendered via twig). If you need to
    make changes you will have to make changes in two places, sorry.
    
    I've also added some variables into the yml files in the different
    environments that were being taken care of via _config/site.yml in
    awestruct.
    
    Lastly, I've updated Drupal to 8.1.2 and also updated some of the
    contrib modules to their latest releases.

--- _ext/jboss_developer.rb
+++ _ext/jboss_developer.rb
@@ -403,7 +403,7 @@ module Aweplug
          _links: {type: {href: File.join(@base_url, '/rest/type/node/', drupal_type)}},
          body: [{value: content,
                  summary: page.description,
-                 format: 'as_is_html'}],
+                 format: 'full_html'}],
          path: {alias: File.join('/', path)}
         }
       end
