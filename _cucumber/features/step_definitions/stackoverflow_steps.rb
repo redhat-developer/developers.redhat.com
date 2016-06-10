be425eb07876f78aff94fab9bb669166c129fcd7
rubygemtsl <ian.hamilton@rubygemtsl.co.uk>
2016-06-10 17:26:50 +0100

    added step

--- _cucumber/features/step_definitions/stackoverflow_steps.rb
+++ _cucumber/features/step_definitions/stackoverflow_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I am on the Stack Overflow page$/) do  
+Given(/^I am on the Stack Overflow page$/) do
   @page.site_nav.visit('/community/stack-overflow')
   @page.site_nav.wait_for_ajax
 end
@@ -8,18 +8,17 @@ Then(/^I should see a list of (\d+) results$/) do |arg|
 end
 
 Then(/^each results should contain an activity summary:$/) do |table|
-  # table is a Cucumber::Core::Ast::DataTable
   table.raw.each do |row|
-    element = row.first
-    case element
-      when 'Votes'
+    element = row.first
+    case element
+      when 'Votes'
         expect(@page.stack_overflow.votes).to eq 10
-      when 'Answers'
+      when 'Answers'
         expect(@page.stack_overflow.answers).to eq 10
-      when 'Views'
+      when 'Views'
         expect(@page.stack_overflow.views).to eq 10
-      else
-        raise("#{element} not regognised")
-      end
-    end
+      else
+        raise("#{element} not regognised")
+    end
+  end
 end
