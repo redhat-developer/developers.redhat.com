074fe3a21ec690775e1ceae43844d973963d14de
Rob Blake <rblake@redhat.com>
2016-08-09 14:56:45 +0100

    [DEVELOPER-3326] Added settings to rhd.settings.php to disable automatic cron

--- _docker/environments/drupal-dev/rhd.settings.php
+++ _docker/environments/drupal-dev/rhd.settings.php
@@ -5,6 +5,7 @@ $settings['container_yamls'][] = DRUPAL_ROOT . '/sites/development.services.yml'
 
 $config['system.performance']['css']['preprocess'] = FALSE;
 $config['system.performance']['js']['preprocess'] = FALSE;
+$config['automated_cron.settings']['interval'] = 0;
 
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
