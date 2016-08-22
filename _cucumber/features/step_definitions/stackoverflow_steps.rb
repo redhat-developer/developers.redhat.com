2f767071d990759f9beef9e261c5c3b32cf7ee67
rubygemtsl <ian.hamilton@rubygemtsl.co.uk>
2016-08-22 14:23:57 +0100

    removed duplicate files and merged my changes to stack steps

--- _cucumber/features/step_definitions/stackoverflow_steps.rb
+++ _cucumber/features/step_definitions/stackoverflow_steps.rb
@@ -1,85 +1,93 @@
 Then(/^I should see a list of (\d+) results$/) do |results|
   @results = results.to_i
-  @page.stack_overflow.questions_loaded?(@results)
+  on StackOverflowPage do |page|
+    page.questions_loaded?(@results)
+  end
 end
 
 Then(/^each results should contain an activity summary:$/) do |table|
-  table.raw.each do |row|
-    type = row.first
-    @page.stack_overflow.activity_summary(type).each { |el| expect(el.text).to eq type }
+  on StackOverflowPage do |page|
+    table.raw.each do |row|
+      type = row.first
+      page.send(type.downcase).each { |el| expect(el.text).to eq type }
+    end
   end
 end
 
 Then(/^each question should contain a question summary$/) do
-  expect(@page.stack_overflow.question_summary.size).to eq @results
+  on StackOverflowPage do |page|
+    expect(page.question_summary.size).to eq @results
+  end
 end
 
 Then(/^each question should link to the relevant question on Stack Overflow$/) do
-  @page.stack_overflow.question_titles.each { |link| expect(link.attribute('href')).to include 'http://stackoverflow.com/questions/' }
+  on StackOverflowPage do |page|
+    page.question_links.each { |link| expect(link.attribute('href')).to include 'http://stackoverflow.com/questions/' }
+  end
 end
 
 When(/^a question contains an answer$/) do
-  @page.stack_overflow.question_with_answer
+  @current_page.get_question_with_answer
 end
 
 When(/^a question does not contain an answer$/) do
-  @page.stack_overflow.question_without_answer
+  @current_page.get_question_without_answer
 end
 
 When(/^I (should|should not) see a "([^"]*)" section$/) do |negate, arg|
   if negate == 'should'
-    expect(@page.stack_overflow.latest_answer_section_visible?).to be true
+    expect(@current_page.latest_answer_section_visible?).to be true
   else
-    expect(@page.stack_overflow.latest_answer_section_visible?).to be false
+    expect(@current_page.latest_answer_section_visible?).to be false
   end
 end
 
 And(/^a "([^"]*)" link that links to that question on Stack Overflow in a new window$/) do |link_title|
-  first_window = @page.stack_overflow.current_window
-  @page.stack_overflow.click_read_full_question_link(link_title)
-  @page.stack_overflow.wait_for_windows(2)
-  @page.stack_overflow.switch_window(first_window)
+  first_window = @current_page.current_window
+  @current_page.click_read_full_question_link(link_title)
+  @current_page.wait_for_windows(2)
+  @current_page.switch_window(first_window)
 end
-
 
 Then(/^each question should display how long ago the question was asked$/) do
-  expect(@page.stack_overflow.author.size).to eq @results
+  expect(@current_page.author.size).to eq @results
 end
 
-
 Then(/^I should see a Filter by product drop down menu with the following:$/) do |table|
-  table.raw.each do |row|
-    product = row.first
-    expect(@page.stack_overflow.product_filter.text).to include product
+  on StackOverflowPage do |page|
+    table.raw.each do |row|
+      product = row.first
+      expect(page.product_filter.text).to include product
+    end
   end
 end
 
 When(/^I select "([^"]*)" from the products filter$/) do |product|
-  @initial_product_titles = []
-  @page.stack_overflow.question_titles.each { |title| @initial_product_titles << title.text }
-  @page.stack_overflow.select_product(product)
+  on StackOverflowPage do |page|
+    @initial_qtn_content  = []
+    page.question_summary.each { |title| @initial_qtn_content << title.text }
+    page.select_product(product)
+  end
 end
 
 Then(/^the results should be updated containing questions relating to "([^"]*)"$/) do |arg|
-  updated_product_titles = []
-  @page.stack_overflow.question_titles.each { |title| updated_product_titles << title.text }
-  updated_product_titles.should_not =~ @initial_product_titles
+  on StackOverflowPage do |page|
+    updated_qtn_content = []
+    page.question_summary.each { |title| updated_qtn_content << title.text }
+    updated_qtn_content.should_not =~ @initial_qtn_content
+  end
 end
 
 Then(/^the default item within the Filter by product drop down menu should be "([^"]*)"$/) do |default|
-  expect(@page.stack_overflow.selected_filter).to eq default
+  on StackOverflowPage do |page|
+    expect(page.selected_filter).to eq default
+  end
 end
 
 Given(/^I have previously filtered results by "([^"]*)"$/) do |product|
-  @initial_product_titles = []
-  @page.stack_overflow.question_titles.each { |title| @initial_product_titles << title.text }
-  @page.stack_overflow.select_product(product)
-end
-
-Given(/^the answers per page options should be:$/) do |table|
-  answers_per_page = []
-  table.raw.each do |links|
-    answers_per_page << links.first
+  on StackOverflowPage do |page|
+    @initial_qtn_content = []
+    page.question_summary.each { |title| @initial_qtn_content << title.text }
+    page.select_product(product)
   end
-  @page.current_page.answers_per_page[1].should =~ answers_per_page
 end
