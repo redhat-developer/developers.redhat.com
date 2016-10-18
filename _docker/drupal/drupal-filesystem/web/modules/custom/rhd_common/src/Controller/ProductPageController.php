aa418dc48923990ff459afbecb1b071f6f6c26b5
LightGuard <lightguard.jp@gmail.com>
2016-10-18 15:54:45 -0600

    Removing deprecation notices

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_common/src/Controller/ProductPageController.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_common/src/Controller/ProductPageController.php
@@ -3,19 +3,43 @@
 namespace Drupal\rhd_common\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Url;
+use Drupal\node\Entity\Node;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Product page controller class definition.
  */
 class ProductPageController extends ControllerBase {
+  /**
+   * @var Connection
+   */
+  private $connection;
+
+  /**
+   * ProductPageController constructor.
+   * @param \Drupal\Core\Database\Connection $connection
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('database'));
+  }
 
   /**
    * Router callback function.
    */
   public function productPage($product_code, $sub_page) {
     // Get node id from the product code.
-    $query = db_select('node__field_product_machine_name', 'pmn');
+    $query = $this->connection->select('node__field_product_machine_name', 'pmn');
     $query->fields('pmn', array('entity_id'));
     $query->condition('pmn.field_product_machine_name_value', $product_code);
     $nid = $query->execute()->fetchField();
@@ -28,18 +52,23 @@ class ProductPageController extends ControllerBase {
     $main_content = '';
     $buzz_tags = '';
     if (!empty($nid)) {
-      $node_obj = node_load($nid);
+      $node_obj = Node::load($nid);
       $title = $node_obj->title->value;
       $summary = $node_obj->field_product_summary->value;
       $get_started = $node_obj->field_product_get_started->value;
       foreach ($node_obj->field_product_pages as $product_page) {
         $product_pages_id = $product_page->target_id;
         // entity_load paragraph type.
-        $load_paragraph = entity_load('paragraph', $product_pages_id);
+        $load_paragraph = $this->entityTypeManager()
+          ->getStorage('paragraph')
+          ->load($product_pages_id);
         // Prepare left nav links data.
         $overview_url = $load_paragraph->field_overview_url->value;
         $url_string = str_replace(' ', '-', strtolower($overview_url));
-        $url = Url::fromRoute('rhd_common.main_page_controller', array('product_code' => $product_code, 'sub_page' => $url_string))->toString();
+        $url = Url::fromRoute('rhd_common.main_page_controller', array(
+          'product_code' => $product_code,
+          'sub_page' => $url_string
+        ))->toString();
         if ($url_string == $sub_page) {
           $active_sub_page = $product_pages_id;
         }
@@ -65,12 +94,14 @@ class ProductPageController extends ControllerBase {
       }
     }
     // Process active sub page and fetch paragraph data.
-    $active_paragraph = entity_load('paragraph', $active_sub_page);
+    $active_paragraph = $this->entityTypeManager()
+      ->getStorage('paragraph')
+      ->load($active_sub_page);
     // Main content data.
     if (isset($active_paragraph->field_overview_main_content)) {
       $main_content = $active_paragraph->field_overview_main_content->value;
     }
-    
+
     // Hide get started block or not.
     $hide_get_started = $active_paragraph->field_overview_hide_get_started->value;
     $nav_url = $active_paragraph->field_overview_url->value;
@@ -79,20 +110,25 @@ class ProductPageController extends ControllerBase {
     if ($hide_get_started && $sub_page = $url_string) {
       $disable = 1;
     }
-    
+
     // If sub-page is docs and apis.
     // Call a different template as the laypout is different.
     if ($active_paragraph->type->target_id == 'docs_and_apis') {
       if (isset($active_paragraph->field_add_documents)) {
         $row = array();
-        foreach($active_paragraph->field_add_documents as $document_paragraph) {
+        foreach ($active_paragraph->field_add_documents as $document_paragraph) {
           $document_urls = array();
-          $document_paragrpah_id = $document_paragraph->target_id;
-          $document_paragraph_data = entity_load('paragraph', $document_paragrpah_id);
+          $document_paragraph_id = $document_paragraph->target_id;
+          $document_paragraph_data = $this->entityTypeManager()
+            ->getStorage('paragraph')
+            ->load($document_paragraph_id);
           $document_title = $document_paragraph_data->field_document_title->value;
           $document_description = $document_paragraph_data->field_document_description->value;
-          foreach($document_paragraph_data->field_document_url as $document_url) {
-            $document_urls[] = array('link_title' => $document_url->title, 'link_url' => $document_url->uri);
+          foreach ($document_paragraph_data->field_document_url as $document_url) {
+            $document_urls[] = array(
+              'link_title' => $document_url->title,
+              'link_url' => $document_url->uri
+            );
           }
           $row[] = array(
             'doc_title' => $document_title,
