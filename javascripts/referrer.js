9fc8975c2f5f0be07f2fdc04f3620d34aae96274
Luke Dary <ldary@redhat.com>
2016-10-28 10:30:33 -0400

    Add messaging to more pages

--- javascripts/referrer.js
+++ javascripts/referrer.js
@@ -1,7 +1,31 @@
 (function() {
   $(function() { 
+    var referrerHTML = $('<section id="referral-alert"><div class="row content"><div class="large-24 columns"><div class="large-24 columns"><div class="alert-box info radius"><h4>You have been redirected from JBoss.org to Red Hat Developers.</h4><p>It'+"'"+'s true — JBoss Developer and Red Hat Developers are one and the same, and you can find all the great stuff you were looking for right here on <a href="http://developers.redhat.com/">developers.redhat.com.</a></p><a class="close">x</a></div></div></div></div></section>');
     if(isReferrer('jbd')) {
-      $('#referral-alert').removeClass('hide');
+      switch (getPrimaryCategory()) {
+        case 'products': // before class .mobile.product-header
+          referrerHTML.insertBefore('.mobile.product-header');
+          break;
+        case 'downloads': // before class most-popular-downloads
+          referrerHTML.insertBefore('.most-popular-downloads');
+          break;
+        case 'containers': // first of class topics-main
+          referrerHTML.insertBefore('.topics-main div:first');
+          break;
+        case 'community': // after class contributors-main
+          referrerHTML.insertAfter('.contributors-main');
+          break;
+        case 'quickstarts': // after id start
+          referrerHTML.insertAfter('#start');
+          break;
+        case 'events': // after class hero
+          referrerHTML.insertAfter('.hero');
+          break;
+        case '':
+          referrerHTML.insertAfter('.spotlights-wrap');
+          break;
+      }
+      
       $('#referral-alert .close').click(function() {
         $('#referral-alert').addClass('hide');
       });
@@ -22,5 +46,9 @@
       referrer = vals['referrer'] === ref;
     }
     return referrer;
+  }
+
+  function getPrimaryCategory() {
+    return window.location.href.split('?')[0].split('/')[3];
   }
 }());
