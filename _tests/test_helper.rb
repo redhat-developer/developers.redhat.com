cc556456f7b851074b0fef249e6068c9d82d9566
rubygemtsl <ian.hamilton@rubygemtsl.co.uk>
2017-01-07 12:52:20 +0000

    wip

--- _tests/test_helper.rb
+++ _tests/test_helper.rb
@@ -1,5 +1,5 @@
-require 'minitest/autorun'
-require 'minitest/reporters' # requires the gem
-require 'webmock/minitest'
-
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
+# require 'minitest/autorun'
+# require 'minitest/reporters' # requires the gem
+# require 'webmock/minitest'
+#
+# Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new # spec-like progress
