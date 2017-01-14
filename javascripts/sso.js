d5a2f08de7462931c89569022bbb3cb26ff3a51e
Paul Robinson <paul.robinson@redhat.com>
2017-01-14 18:13:24 +0000

    Revert "Fixes for issues found during analytics training"

--- javascripts/sso.js
+++ javascripts/sso.js
@@ -159,7 +159,7 @@ app.sso = function () {
         saveTokens();
         checkIfProtectedPage();
 
-        if ($('.downloadthankyou:visible').length && app.termsAndConditions) {
+        if ($('.downloadthankyou').length && app.termsAndConditions) {
             app.termsAndConditions.download();
         }
         
