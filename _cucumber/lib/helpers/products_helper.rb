8d2812a50c1c25a54a947aa908eb7e7ba1fea7c6
Rob Blake <rblake@redhat.com>
2017-01-24 13:30:40 +0000

    [DEVELOPER-3761] Parameterised the location of directories used within testing

--- _cucumber/lib/helpers/products_helper.rb
+++ _cucumber/lib/helpers/products_helper.rb
@@ -3,7 +3,7 @@ module ProductsHelper
   module_function
 
   def product(product_id, info)
-    data = YAML.load_file("products/#{product_id}/_common/product.yml")
+    data = YAML.load_file("#{$products_dir}/#{product_id}/_common/product.yml")
     case info
       when 'name'
         data['name']
@@ -21,7 +21,7 @@ module ProductsHelper
   def products
     product_type = []
     product_id = []
-    Dir.glob(File.join('products', '**', '**.yml')).each do |file|
+    Dir.glob(File.join("#{$products_dir}", '**', '**.yml')).each do |file|
       data = YAML.load_file(file)
       product_type << data['name']
       product_id << file.sub('products/', '').sub('/_common/product.yml', '')
@@ -31,14 +31,14 @@ module ProductsHelper
   end
 
   def product_by_id(id)
-    data = YAML.load_file("products/#{id}/_common/product.yml")
+    data = YAML.load_file("#{$products_dir}/#{id}/_common/product.yml")
     data['name']
   end
 
   def products_with_links(link_type)
     product_type = []
     product_id = []
-    if Dir.glob(File.join('products', '**', link_type)).each do |learn|
+    if Dir.glob(File.join("#{$product_dir}", '**', link_type)).each do |learn|
       path_to_product = learn.gsub("/#{link_type}", '')
       Dir.glob("#{path_to_product}/_common/product.yml").each do |f|
         product_id << path_to_product.sub('products/', '')
@@ -54,7 +54,7 @@ module ProductsHelper
   def categories
     product_title = []
     product_id = []
-    data = YAML.load_file('_config/categories.yml')
+    data = YAML.load_file("#{$config_dir}/categories.yml")
     data.each do |product|
       product_title << product['name']
       product_id << product['products']
