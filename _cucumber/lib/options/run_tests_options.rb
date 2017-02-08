7a0494882b94a024f1893b85cea0d94632f3ce9c
rubygemtsl <ian.hamilton@rubygemtsl.co.uk>
2017-02-08 20:51:14 +0000

    rebasing, fixed unit tests.

--- _cucumber/lib/options/run_tests_options.rb
+++ _cucumber/lib/options/run_tests_options.rb
@@ -161,7 +161,7 @@ class RunTestsOptions
       #
       docker_driver = @supported_drivers.include?(driver) ? "#{driver}" : 'chrome'
       driver = @supported_drivers.include?(driver) ? "#{driver}" : driver
-      bind_environment_variable('RHD_DOCKER_DRIVER', docker_driver)
+      bind_environment_variable('RHD_DOCKER_DRIVER', "docker_#{docker_driver}")
       test_configuration[:docker_node] = "docker_#{docker_driver}"
     end
 
