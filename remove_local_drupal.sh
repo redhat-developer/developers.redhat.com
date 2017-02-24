adccdba30ab18c88642c02abc634120b82dbf62c
LightGuard <lightguard.jp@gmail.com>
2017-02-24 11:56:50 -0700

    Adding the shell scripts
    Setup and remove scripts

---
+++ remove_local_drupal.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Modify these for your system and setup
+USER="jporter"
+GROUP=$USER
+PROJ="/home/${USER}/projects/developers.redhat.com"
+
+# These locations should not need to change
+DRUPAL="${PROJ}/_docker/drupal"
+DRUPAL_FILESYSTEM="${DRUPAL}/drupal-filesystem"
+WEB="${DRUPAL_FILESYSTEM}/web"
+ENVIRON="${PROJ}/_docker/environments/drupal-dev"
+
+echo "Resetting workspace"
+sudo git reset --hard HEAD
+
+ecoh "Removing untracked files"
+sudo git clean -dfi
+
---
+++ setup_local_drupal.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Modify these for your system and setup
+USER="jporter"
+GROUP=$USER
+PROJ="/home/${USER}/projects/developers.redhat.com"
+
+# These locations should not need to change
+DRUPAL="${PROJ}/_docker/drupal"
+DRUPAL_FILESYSTEM="${DRUPAL}/drupal-filesystem"
+WEB="${DRUPAL_FILESYSTEM}/web"
+ENVIRON="${PROJ}/_docker/environments/drupal-dev"
+
+# Getting the database going, because we can't really do much until this is up
+docker-compose -f "${ENVIRON}/docker-compose.yml" up -d drupalmysql
+
+echo "Waiting until the database is up..."
+
+php <<'DBCHECK'
+<?php
+
+$conn = NULL;
+while ($conn === NULL) {
+    try {
+        $conn = new PDO('mysql:host=docker;dbname=drupal', 'drupal', 'drupal');
+        $conn->query('SELECT 1;');
+    } catch (PDOException $exception) {
+        sleep(5);
+    }
+}
+$conn = NULL;
+DBCHECK
+
+# Symlink scripts so we can run composer
+if [ ! -d "${DRUPAL_FILESYSTEM}/scripts" ]
+then
+  ln -s ${DRUPAL}/scripts ${DRUPAL_FILESYSTEM}/scripts
+fi
+
+echo "Running composer install"
+
+# Install Drupal
+cd ${DRUPAL_FILESYSTEM}
+composer install
+
+# Symlink the rest of the files and folders needed
+echo "Creating symlinks if needed" 
+
+if [ ! -d "${WEB}/fonts" ]
+then
+  ln -s ${PROJ}/stylesheets/fonts ${WEB}/fonts
+fi
+
+if [ ! -d "${WEB}/images" ]
+then
+  ln -s ${PROJ}/images ${WEB}/images
+fi
+
+
+if [ ! -f "${WEB}/sites/default/rhd.settings.php" ]
+then
+  sudo ln -s ${ENVIRON}/rhd.settings.php ${WEB}/sites/default/rhd.settings.php
+fi
+
+if [ ! -f "${WEB}/sites/default/rhd.settings.yml" ]
+then
+  sudo ln -s ${ENVIRON}/rhd.settings.yml ${WEB}/sites/default/rhd.settings.yml
+fi
+
+# ADD from docker active config, files
+echo "Adding the active config from the prod dump"
+rm -rf ${WEB}/config/active
+mkdir ${WEB}/config/active
+
+sudo docker cp drupaldev_drupal_data_1:/var/www/drupal/web/config/active ${WEB}/config
+sudo chown -R ${USER}:${GROUP} ${WEB}/config/active
+
+echo "Adding site/files"
+sudo docker cp drupaldev_drupal_data_1:/var/www/drupal/web/sites/default/files ${WEB}/sites/default
+sudo chown -R ${USER}:${GROUP} ${WEB}/sites/default/files
+
+# Import config and update the database
+echo "Running drush updb"
+${WEB}/../vendor/bin/drush --root=${WEB} updb --entity-updates
+
+echo "Running drush cim"
+${WEB}/../vendor/bin/drush --root=${WEB} cim
+
+echo "Starting the server"
+${WEB}/../vendor/bin/drush --root=${WEB} rs
+
