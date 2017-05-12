cce93d61973d45bdf759df28d3ee5a83b9a4ad2f
LightGuard <lightguard.jp@gmail.com>
2017-05-12 15:13:55 -0600

    SQL fix

--- _docker/drupal/drupal-filesystem/db-migrations/20170512203354_rhamt_fix.php
+++ _docker/drupal/drupal-filesystem/db-migrations/20170512203354_rhamt_fix.php
@@ -10,8 +10,8 @@ class RhamtFix extends AbstractMigration
   public function up() {
     $this->execute('UPDATE node_field_data SET _deleted = 1 WHERE nid IN (37115, 37135)');
     $this->execute('UPDATE node_field_revision SET _deleted = 1 WHERE nid IN (37115, 37135)');
-    $this->execute("UPDATE node__field_url_product_name SET field_url_product_name_value = 'rhamt' WHERE nid = 37375");
-    $this->execute("UPDATE node_revision__field_url_product_name SET field_url_product_name_value = 'rhamt' WHERE nid = 37375");
+    $this->execute("UPDATE node__field_url_product_name SET field_url_product_name_value = 'rhamt' WHERE entity_id = 37375");
+    $this->execute("UPDATE node_revision__field_url_product_name SET field_url_product_name_value = 'rhamt' WHERE entity_id = 37375");
     $this->execute("UPDATE node_field_revision SET moderation_state = 'published' WHERE nid = 37375;");
     $this->execute("UPDATE node_field_data SET moderation_state = 'published' WHERE nid = 37375;");
   }
@@ -22,8 +22,8 @@ class RhamtFix extends AbstractMigration
   public function down() {
     $this->execute('UPDATE node_field_data SET _deleted = 0 WHERE nid IN (37135)'); // 37115 was also broken, or deleted before, leaved it deleted
     $this->execute('UPDATE node_field_revision SET _deleted = 0 WHERE nid IN (37135)');
-    $this->execute("UPDATE node__field_url_product_name SET field_url_product_name_value = 'rhamt-new' WHERE nid = 37375");
-    $this->execute("UPDATE node_revision__field_url_product_name SET field_url_product_name_value = 'rhamt-new' WHERE nid = 37375");
+    $this->execute("UPDATE node__field_url_product_name SET field_url_product_name_value = 'rhamt-new' WHERE entity_id = 37375");
+    $this->execute("UPDATE node_revision__field_url_product_name SET field_url_product_name_value = 'rhamt-new' WHERE entity_id = 37375");
     $this->execute("UPDATE node_field_revision SET moderation_state = 'draft' WHERE nid = 37375;");
     $this->execute("UPDATE node_field_data SET moderation_state = 'draft' WHERE nid = 37375;");
   }
