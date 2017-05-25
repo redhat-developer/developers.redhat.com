7566b8e46664829d33ff647977d809cd482de930
Luke Dary <ldary@redhat.com>
2017-05-25 19:52:51 -0400

    Latest updates to Search Components

--- _tests/karma/rhdp-search/rhdp-search-filter-group_spec.js
+++ _tests/karma/rhdp-search/rhdp-search-filter-group_spec.js
@@ -11,6 +11,27 @@ describe('Search Filter Group', function() {
         document.body.removeChild(document.body.firstChild);
     });
 
+    describe('Heading', function() {
+        it('should display the name in all CAPS', function() {
+            var head = 'Test Heading';
+            wc.name = head;
+            expect(wc.shadowRoot.querySelector('h4').innerText).toEqual(head.toUpperCase());
+        });
+    });
+
+    describe('Filter Items', function() {
+
+    });
+
+    describe('Toggle', function() {
+        it('should change the class of filters container', function() {
+            var t = 'filters show', f = 'filters hide';
+            expect(wc.shadowRoot.querySelector('#filters').className).toBe(f);
+            wc.showFilters();
+            expect(wc.shadowRoot.querySelector('#filters').className).toBe(t);
+        });
+    });
+
     it('should be true', function() {
         expect(wc.innerText).toEqual('');
     });
--- _tests/karma/rhdp-search/rhdp-search-filter-item_spec.js
+++ _tests/karma/rhdp-search/rhdp-search-filter-item_spec.js
@@ -11,7 +11,57 @@ describe('Search Filter Item', function() {
         document.body.removeChild(document.body.firstChild);
     });
 
-    it('should be true', function() {
-        expect(wc.innerText).toEqual('');
+    describe('Text label', function() {
+        it('should show the name field', function() {
+            wc.set('name','Test Name');
+            expect(wc.shadowRoot.querySelector('div').innerText).toEqual('Test Name');
+        });
+        it('should toggle the control when clicked', function() {
+            wc.shadowRoot.querySelector('div').click();
+            expect(wc.toggle).toBe(true);
+            wc.shadowRoot.querySelector('div').click();
+            expect(wc.toggle).toBe(false);
+        })
     });
+
+    describe('Checkbox', function() {
+        it('should set the toggle', function() {
+            var t = wc.toggle;
+            wc.shadowRoot.querySelector('paper-checkbox').checked = true;
+            expect(wc.toggle).toBe(true);
+            wc.shadowRoot.querySelector('paper-checkbox').checked = false;
+            expect(wc.toggle).toBe(false);
+        });
+    });
+
+    describe('Toggle', function() {
+        it('should set the toggle', function() {
+            wc.shadowRoot.querySelector('paper-checkbox').checked = true;
+            expect(wc.toggle).toBe(true);
+            wc.shadowRoot.querySelector('paper-checkbox').checked = false;
+            expect(wc.toggle).toBe(false);
+        });
+    });
+
+    describe('setToggle Function', function() {
+        it('should set the toggle', function() {
+            expect(wc.toggle).toBe(false);
+            wc.setToggle();
+            expect(wc.toggle).toBe(true);
+            wc.setToggle();
+            expect(wc.toggle).toBe(false);
+        })
+    });
+
+    describe('clear Function', function() {
+        it('should clear the toggle', function() {
+            expect(wc.toggle).toBe(false);
+            wc.clear();
+            expect(wc.toggle).toBe(false);
+            wc.toggle = true;
+            wc.clear();
+            expect(wc.toggle).toBe(false);
+        })
+    });
+    
 });
