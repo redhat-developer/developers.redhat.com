5f79e7ab0b957e2af2a91e49bb9ea039c707ce52
Luke Dary <ldary@redhat.com>
2017-06-15 11:32:47 -0400

    Add redirect URL and not redirect on Drupal login

--- javascripts/sso.js
+++ javascripts/sso.js
@@ -3,7 +3,7 @@ app.sso = function () {
     function updateUser() {
         var usr = digitalData.user[0].profile[0].profileInfo;
 
-        if (window.location.href.indexOf('/login') >= 0) {
+        if (window.location.href.indexOf('/login') >= 0 && window.location.href.indexOf('/user') < 0) {
             keycloak.login({"redirectUri": app.ssoConfig.logout_url});
         }
 
