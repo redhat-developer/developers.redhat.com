2bbc61edf2be561d31160cf12d82117a93389619
Ian Hamilton <ian.hamilton1@digital.hmrc.gov.uk>
2017-08-15 10:55:32 +0100

    DEVELOPER-4360 - Fix for failing nightly broken link checks in production.

--- _tests/node_jenkins_test_runner.rb
+++ _tests/node_jenkins_test_runner.rb
@@ -6,19 +6,25 @@ require_relative '../_docker/lib/process_runner'
 #
 class NodeJenkinsTestRunner
 
-  def initialize(host_to_test, control_script_directory, process_runner)
+  def initialize(test_type, host_to_test, control_script_directory, process_runner)
+    @test_type = test_type
     @host_to_test = host_to_test
     @control_script_directory = control_script_directory
     @process_runner = process_runner
   end
 
   #
-  # Run the tests for each of our test profiles
+  # Run the tests based on user specified test type
   #
   def run_tests
     tests_passed = true
-    %w(desktop mobile).each do |profile|
-      tests_passed &= execute_test(profile)
+    if @test_type == 'e2e'
+      # Run the tests for each of our test profiles
+      %w(desktop mobile).each do |profile|
+        tests_passed &= execute_e2e(profile)
+      end
+    else
+      tests_passed &= execute_blc
     end
     tests_passed
   end
@@ -26,16 +32,31 @@ class NodeJenkinsTestRunner
   private
 
   #
-  # Execute the tests for the given profile
+  # Execute the e2e tests for the given profile
   #
-  def execute_test(profile)
+  def execute_e2e(profile)
     success = true
-    test_execution_command = build_run_tests_command(profile)
+    test_execution_command = build_e2e_run_tests_command(profile)
 
     begin
       @process_runner.execute!(test_execution_command)
     rescue
       puts "Run of test profile '#{profile}' failed."
+      success = false
+    end
+    success
+  end
+
+  #
+  # Execute the blc tests for the given profile
+  #
+  def execute_blc
+    success = true
+    test_execution_command = build_blc_run_tests_command
+    begin
+      @process_runner.execute!(test_execution_command)
+    rescue
+      puts 'Run of blc failed.'
       success = false
     end
     success
@@ -50,10 +71,10 @@ class NodeJenkinsTestRunner
   end
 
   #
-  # Builds the command to use to execute the tests, including whether or not we should send updates to GitHub and any Cucumber tags
+  # Builds the command to use to execute the e2e tests, including whether or not we should send updates to GitHub and any Cucumber tags
   # that should be applied
   #
-  def build_run_tests_command(profile)
+  def build_e2e_run_tests_command(profile)
     command = "ruby _tests/run_tests.rb --e2e --use-docker --base-url=#{@host_to_test}"
     github_sha1 = read_env_variable('ghprbActualCommit')
     cucumber_tags = read_env_variable('CUCUMBER_TAGS')
@@ -73,6 +94,18 @@ class NodeJenkinsTestRunner
     command
   end
 
+  #
+  # Builds the command to use to execute the blc tests, including whether or not we should send updates to GitHub
+  #
+  def build_blc_run_tests_command
+    github_sha1 = read_env_variable('ghprbActualCommit')
+    config = read_env_variable('CONFIG')
+    command = "ruby _tests/run_tests.rb --blc -c #{config} --base-url=#{@host_to_test}"
+    command += " --update-github-status=#{github_sha1}" if github_sha1
+    command += ' --use-docker'
+    command
+  end
+
 end
 
 #
@@ -85,12 +118,19 @@ end
 
 if $PROGRAM_NAME == __FILE__
 
-  host_to_test = ARGV[0]
+  test_type = ARGV[0]
+  if test_type.nil? || test_type.empty?
+    puts 'Please specify the host to tests you wish to run. Can be e2e or blc'
+    Kernel.exit(1)
+  end
+
+  host_to_test = ARGV[1]
   if host_to_test.nil? || host_to_test.empty?
     puts 'Please specify the host to test as the first argument to this script e.g. ruby jenkins_test_runner.rb https://developers.redhat.com'
     Kernel.exit(1)
   end
 
-  jenkins_test_runner = NodeJenkinsTestRunner.new(host_to_test, "#{__dir__}", ProcessRunner.new)
+  jenkins_test_runner = NodeJenkinsTestRunner.new(test_type, host_to_test, "#{__dir__}", ProcessRunner.new)
   execute(jenkins_test_runner)
 end
+
