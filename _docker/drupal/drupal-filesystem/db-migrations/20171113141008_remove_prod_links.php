3e768f74d762526212e029506745b0725cf1e54d
robpblake <rob@rb-associates.co.uk>
2017-11-14 12:57:54 +0000

    [DEVELOPER-4094] Updated content within the lightning tables as that now appears to be the active profile

--- _docker/drupal/drupal-filesystem/db-migrations/20171113141008_remove_prod_links.php
+++ _docker/drupal/drupal-filesystem/db-migrations/20171113141008_remove_prod_links.php
@@ -7,5 +7,7 @@ class RemoveProdLinks extends AbstractMigration
     public function up() {
         $this->execute('UPDATE node_revision__body SET body_value = REPLACE(body_value, \'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
         $this->execute('UPDATE node__body SET body_value = REPLACE(body_value, \'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
+        $this->execute('UPDATE lightning_node__body SET body_value = REPLACE(body_value, \'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
+        $this->execute('UPDATE lightning_node_revision__body SET body_value = REPLACE(body_value, \'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
     }
 }
