b053fc555b07364a02c9daf6e890b93f993e6f8c
robpblake <rob@rb-associates.co.uk>
2017-11-15 12:21:02 +0000

    [DEVELOPER-4094] Ensured we also update the revision tables for the paragraph content

--- _docker/drupal/drupal-filesystem/db-migrations/20171113141008_remove_prod_links.php
+++ _docker/drupal/drupal-filesystem/db-migrations/20171113141008_remove_prod_links.php
@@ -10,6 +10,8 @@ class RemoveProdLinks extends AbstractMigration
         $this->execute('UPDATE lightning_node_revision__body SET body_value = REPLACE(body_value, \'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
         $this->execute('UPDATE lightning_node_revision__body SET body_value = REPLACE(body_value, \'https://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
         $this->execute('UPDATE lightning_paragraph__field_overview_main_content set field_overview_main_content_value = REPLACE(field_overview_main_content_value,\'https://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
-        $this->execute('UPDATE lightning_paragraph__field_overview_main_content set field_overview_main_content_value = REPLACE(field_overview_main_content_value,\'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
+        $this->execute('UPDATE lightning_paragraph__field_overview_main_content set field_overview_main_content_value = REPLACE(field_overview_main_content_value,\'https://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
+        $this->execute('UPDATE lightning_paragraph_revision__field_overview_main_content set field_overview_main_content_value = REPLACE(field_overview_main_content_value,\'https://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
+        $this->execute('UPDATE lightning_paragraph_revision__field_overview_main_content set field_overview_main_content_value = REPLACE(field_overview_main_content_value,\'http://developer-drupal.web.prod.ext.phx2.redhat.com\', \'\')');
     }
 }
