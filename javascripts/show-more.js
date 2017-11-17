c00174283eb881047f10e4fea456fdbbfaedce0a
Adela Sofia Arreola <aarreola@redhat.com>
2017-11-17 08:06:37 -0700

    DEVELOPER-3890 Improved video pages

---
+++ javascripts/show-more.js
@@ -0,0 +1,18 @@
+var showMoreText = $('.show-more-text').text();
+
+// Minimum number of characters needed to display "Show More" button
+var charCount = $('.show-more-text').attr('data-ccount');
+
+if (showMoreText.length < charCount){
+  $('a.show-more').hide();
+}
+
+$('.show-more').on('click', function() {
+
+  var x = $(this);
+  var $showMoreBtn = x.find('span');
+  var $showMoreContent = x.prev();
+  $showMoreContent.toggleClass('open');
+
+  return $showMoreBtn.toggle();
+});
