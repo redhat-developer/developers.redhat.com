dfd6f876ad6a9c978a8416fbf346d70ce96a5470
dcoughlin <daniel.coughlin86@gmail.com>
2017-12-12 11:46:13 -0800

    DEVELOPER-4526 - Added more fields to search/replace

---
+++ _docker/drupal/drupal-filesystem/db-migrations/20171129172648_rename_redhat_developers.php
@@ -0,0 +1,31 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class RenameRedhatDevelopers extends AbstractMigration
+{
+
+    // Update all instances of Red Hat Developers with Red Hat Developer Program
+    public function up() {
+        $this->execute('UPDATE lightning_node__body SET body_value = REPLACE(body_value, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_node_field_data SET title = REPLACE(title, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_node__field_speakers SET field_speakers_value = REPLACE(field_speakers_value, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_node__body SET body_summary = REPLACE(body_summary, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_node__field_content_author SET field_content_author_value = REPLACE(field_content_author_value, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_paragraph__field_body SET field_body_value = REPLACE(field_body_value, "Red Hat Developers", "Red Hat Developer Program")');
+        $this->execute('UPDATE lightning_paragraph__field_overview_main_content SET field_overview_main_content_value = REPLACE(field_overview_main_content_value, "Red Hat Developers", "Red Hat Developer Program")');
+
+    }
+
+    public function down() {
+        $this->execute('UPDATE lightning_node__body SET body_value = REPLACE(body_value, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_node_field_data SET title = REPLACE(title, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_node__field_speakers SET field_speakers_value = REPLACE(field_speakers_value, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_node__body SET body_summary = REPLACE(body_summary, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_node__field_content_author SET field_content_author_value = REPLACE(field_content_author_value, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_paragraph__field_body SET field_body_value = REPLACE(field_body_value, "Red Hat Developer Program", "Red Hat Developers")');
+        $this->execute('UPDATE lightning_paragraph__field_overview_main_content SET field_overview_main_content_value = REPLACE(field_overview_main_content_value, "Red Hat Developer Program", "Red Hat Developers")');
+
+    }
+
+}
