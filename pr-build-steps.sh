cfb13f36d56eaca24d34e95a9e6b416cbc5fd684
LightGuard <lightguard.jp@gmail.com>
2018-05-14 16:34:02 -0600

    Small refactor

--- pr-build-steps.sh
+++ pr-build-steps.sh
@@ -1,7 +1,10 @@
 #!/bin/bash --login -e
 set -e
+
+# Needed so we can pull data-container-lite
 docker login --username $DOCKER_HUB_USERNAME --password $DOCKER_HUB_PASSWORD
 
+# Needed for rsync
 cp /home/jenkins_developer/id_rsa $WORKSPACE/_docker/awestruct/overlay/ssh-key/
 cp /home/jenkins_developer/known_hosts $WORKSPACE/_docker/awestruct/overlay/ssh-key/
 
@@ -11,7 +14,9 @@ cd _docker/
 bundle install
 ruby ./control.rb -e drupal-pull-request --run-the-stack --no-decrypt
 
-drupal_status=$(docker logs rhdpr${ghprbPullId}_drupal_1 | tail -n 1 | grep 'Success')
+drupal_port=$((35000 + $ghprbPullId))
+drupal_url="http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:${drupal_port}"
+drupal_status=$(curl -XHEAD ${drupal_url})
 
 # Update GitHub with the new Drupal Link
 if [ $drupal_status -ne 0 ]
@@ -19,15 +24,15 @@ then
   curl -XPOST -H "Authorization: token ${github_status_api_token}" https://api.github.com/repos/redhat-developer/developers.redhat.com/statuses/${ghprbActualCommit} -d "{
     \"state\": \"failure\",
     \"context\": \"drupal-site\",
-    \"target_url\": \"http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:$((35000 + $ghprbPullId))\",
-    \"description\": \"$(docker logs rhdpr${ghprbPullId_drupal_1} | tail -n 3)\"
+    \"target_url\": \"${drupal_url}\",
+    \"description\": \"$(docker logs rhdpr${ghprbPullId}_drupal_1 | tail -n 3)\"
   }"
   exit 1 # exit, no point continuing
 else
   curl -XPOST -H "Authorization: token ${github_status_api_token}" https://api.github.com/repos/redhat-developer/developers.redhat.com/statuses/${ghprbActualCommit} -d "{
     \"state\": \"success\",
     \"context\": \"drupal-site\",
-    \"target_url\": \"http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:$((35000 + $ghprbPullId))\",
+    \"target_url\": \"${drupal_url}\",
     \"description\": \"finished ok!\"
   }"
 fi
