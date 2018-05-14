7fff5e34f0a9b19da6653ab3f42da85c5090e43e
LightGuard <lightguard.jp@gmail.com>
2018-05-14 15:29:11 -0600

    Adding check for drupal being up

--- pr-build-steps.sh
+++ pr-build-steps.sh
@@ -11,13 +11,26 @@ cd _docker/
 bundle install
 ruby ./control.rb -e drupal-pull-request --run-the-stack --no-decrypt
 
+drupal_status=$(docker logs rhdpr$ghprbPullId_drupal_1 | tail -n 1 | grep 'Success')
+
 # Update GitHub with the new Drupal Link
-curl -XPOST -H "Authorization: token $github_status_api_token" https://api.github.com/repos/redhat-developer/developers.redhat.com/statuses/$ghprbActualCommit -d "{
-  \"state\": \"success\",
-  \"context\": \"drupal-site\",
-  \"target_url\": \"http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:$((35000 + $ghprbPullId))\",
-  \"description\": \"finished ok! (from jenkins)\"
-}"
+if [ $drupal_status -ne 0 ]
+then
+  curl -XPOST -H "Authorization: token $github_status_api_token" https://api.github.com/repos/redhat-developer/developers.redhat.com/statuses/$ghprbActualCommit -d "{
+    \"state\": \"failure\",
+    \"context\": \"drupal-site\",
+    \"target_url\": \"http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:$((35000 + $ghprbPullId))\",
+    \"description\": \"$(docker logs rhdpr$ghprbPullId_drupal_1 | tail -n 3)\"
+  }"
+  exit 1 # exit, no point continuing
+else
+  curl -XPOST -H "Authorization: token $github_status_api_token" https://api.github.com/repos/redhat-developer/developers.redhat.com/statuses/$ghprbActualCommit -d "{
+    \"state\": \"success\",
+    \"context\": \"drupal-site\",
+    \"target_url\": \"http://rhdp-jenkins-slave.lab4.eng.bos.redhat.com:$((35000 + $ghprbPullId))\",
+    \"description\": \"finished ok!\"
+  }"
+fi
 
 # Start the export process
 ruby ./control.rb -e drupal-pull-request --export rhd@filemgmt.jboss.org:/stg_htdocs/it-rhd-stg[/pr/${ghprbPullId}/export] --no-decrypt --no-kill
