40715ecbb31c8a930176cd3369d442d45ff2690d
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2018-07-24 20:13:48 +0100

    js missed from previous commit

--- _docker/drupal/drupal-filesystem/web/themes/custom/rhdp/rhd-frontend/src/scripts/@rhd/js/line-clamp-binding.js
+++ _docker/drupal/drupal-filesystem/web/themes/custom/rhdp/rhd-frontend/src/scripts/@rhd/js/line-clamp-binding.js
@@ -7,10 +7,12 @@ $(function() {
         var $tmpItem = $(this);
         var testClass = $tmpItem.attr("class");
         var lineCountArray = testClass.split("-");
-        //Grab last item from class and use as numebr of lines to clamp
+        //Grab last item from class and use as numebr of lines to clamp parse as int
         var testLineCount = parseInt(lineCountArray[lineCountArray.length - 1], 10);
 
+        //only run if we have a number of lines to clamp
         if(!isNaN(testLineCount)) {
+            //limit line clamp from 1 to 10 lines
             if(testLineCount <= 10 && testLineCount >= 1) {
                 $clamp($tmpItem.get(0), {clamp: testLineCount});
             }
