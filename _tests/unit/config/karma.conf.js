4870011a0a1aa20883abfd6550b588ee4448c998
Luke Dary <ldary@redhat.com>
2018-08-07 10:08:49 -0400

    Trying to get unit tests to pass

--- _tests/unit/config/karma.conf.js
+++ _tests/unit/config/karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
             'system-production.js', // 'https://cdnjs.cloudflare.com/ajax/libs/systemjs/0.21.4/system-production.js',
             'custom-elements-es5-adapter.js', // 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.0.2/custom-elements-es5-adapter.js',
             'webcomponents-bundle.js', // 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.0.2/webcomponents-bundle.js',
-            '../../../_docker/drupal/drupal-filesystem/web/themes/custom/rhdp/rhd-frontend/rhd.js',
+            '../../../_docker/drupal/drupal-filesystem/web/themes/custom/rhdp/rhd-frontend/rhd.min.js',
             '../../../_docker/drupal/drupal-filesystem/web/themes/custom/rhdp/js/init.js',
             '../**/*_spec.js'          
         ]
--- javascripts/devnation2015.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    var ytThumbs = $('#ytThumbs');
-    if(ytThumbs.length) {
-        ytEmbed.init({'block':'ytThumbs','q':'PLnLQldO10NUMqPvW5loz6aLGL3GrqJEGq','type':'playlist','results':50,'meta':true,'player':'link','layout':'full'});
-    }
-});
