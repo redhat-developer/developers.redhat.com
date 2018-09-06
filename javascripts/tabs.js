81db9416238ef66bfe44fe090aaf8c443438019e
Adela Sofia Arreola <adelaarreola@gmail.com>
2018-09-06 11:55:33 -0600

    Removed outdated javascript dir

--- javascripts/tabs.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var tab = document.querySelector('.drupal-header .tab');
-if (tab !== null) {
-    tab.addEventListener('click', function () {
-        this.parentElement.classList.toggle('open');
-    });
-}
