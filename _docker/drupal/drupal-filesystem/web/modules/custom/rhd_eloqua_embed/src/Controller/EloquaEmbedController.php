bc083a7b2e75a923351f9474ad3850d3dbd55a57
Roger Sullivan <roger@alloymagnetic.com>
2018-09-20 11:54:41 -0600

    Finish Eloqua form embed. Replace form assembly with Eloqua Form Embed assembly. Added to SOLP and Landing Page templates.

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
@@ -10,30 +10,13 @@ use Drupal\assembly\Entity\Assembly;
 
 class EloquaEmbedController {
 
-  public function getForm($assembly_id) { //2548
+  public function getForm($assembly_id) {
     $entity = Assembly::load($assembly_id);
     if($entity->hasField('field_eloqua_json')) {
       $eloqua_json = $entity->get('field_eloqua_json')->value;
       $build['#title'] = $eloqua_json;
       return $build;
     }
-
-    // $eloqua_json = '{
-    //   "showAdditionalFields": [],
-    //   "hideStandardFields": [],
-    //   "CustomQuestions": [],
-    //   "offer_id": "70160000000Qf1SAAS",
-    //   "language": "en",
-    //   "FormTitle": "Sallie Test form ",
-    //   "FormIntro": "This is a test intro paragraph ",
-    //   "NameOrder": "western",
-    //   "FormCallToAction": "DOWNLOAD NOW",
-    //   "ThanksTitle": "Thank you",
-    //   "ThanksText": "Thank you paragraph",
-    //   "ShowThanksButton": "true",
-    //   "leadActivity": 1,
-    //   "disableVisitorContactLookups": false
-    // }';
   }
 
 }
