fb67ed6b69b03fb4aa4345dcc434c6c37d3d1c10
Roger Sullivan <roger@alloymagnetic.com>
2018-09-20 13:50:50 -0600

    Update rhd_eloqua_embed to use renderRoot with module theme template.

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
@@ -7,16 +7,26 @@
 namespace Drupal\rhd_eloqua_embed\Controller;
 
 use Drupal\assembly\Entity\Assembly;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class EloquaEmbedController {
 
-  public function getForm($assembly_id) {
-    $entity = Assembly::load($assembly_id);
-    if($entity->hasField('field_eloqua_json')) {
-      $eloqua_json = $entity->get('field_eloqua_json')->value;
-      $build['#title'] = $eloqua_json;
-      return $build;
+  public function getForm(Assembly $assembly) {
+    if($assembly->hasField('field_eloqua_json')) {
+      $eloqua_json = $assembly->get('field_eloqua_json')->value;
+      if(!$eloqua_json) {
+        throw new NotFoundHttpException();
+      }
+      $build = [
+        '#theme' => 'rhd_eloqua_embed_page',
+        '#eloqua_json' => $eloqua_json,
+      ];
+      $output = \Drupal::service('renderer')->renderRoot($build);
+      $response = new Response();
+      $response->setContent($output);
+      return $response;
     }
+    throw new NotFoundHttpException();
   }
-
 }
