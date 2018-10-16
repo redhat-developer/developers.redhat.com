06749d3229d7036e9f795f66d971d63afd36d467
Roger Sullivan <roger@alloymagnetic.com>
2018-10-16 11:29:49 -0600

    Merge branch 'DEVELOPER-5412' of https://github.com/kerrivader/developers.redhat.com into DEVELOPER-5412

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_eloqua_embed/src/Controller/EloquaEmbedController.php
@@ -9,11 +9,14 @@ namespace Drupal\rhd_eloqua_embed\Controller;
 use Drupal\assembly\Entity\Assembly;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Template\Attribute;
+use Drupal\Component\Utility\Html;
 
 class EloquaEmbedController {
 
   public function getForm(Assembly $assembly) {
     if($assembly->hasField('field_eloqua_json')) {
+      $options = $_GET['options'];
       $eloqua_json = $assembly->get('field_eloqua_json')->value;
       if(!$eloqua_json) {
         throw new NotFoundHttpException();
@@ -22,6 +25,13 @@ class EloquaEmbedController {
         '#theme' => 'rhd_eloqua_embed_page',
         '#eloqua_json' => $eloqua_json,
       ];
+      if (is_array($options) && isset($options['class'])) {
+        $attributes = new Attribute(['class' => $options['class']]);
+        foreach ($options['class'] as $class) {
+          $attributes->addClass(Html::cleanCssIdentifier($class));
+        }
+        $build['#html_attributes'] = $attributes;
+      }
       $output = \Drupal::service('renderer')->renderRoot($build);
       $response = new Response();
       $response->setContent($output);
