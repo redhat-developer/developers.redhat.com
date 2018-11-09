20ec1715445dbbbe137083ddb71c705c2511f90d
Ian Hamilton <ian.hamilton@dnata.com>
2018-11-09 14:23:28 +0400

    removed wip tag

--- _tests/e2e/specs/export/rhd-downloads.spec.js
+++ _tests/e2e/specs/export/rhd-downloads.spec.js
@@ -40,7 +40,7 @@ tags('desktop').describe('Download Manager', function () {
         });
 
     tags('dm')
-        .it('@sanity @wip: should allow users to log-in and download advanced-linux-commands', function () {
+        .it('@sanity : should allow users to log-in and download advanced-linux-commands', function () {
             let downloadDir = new DownloadDir();
             let siteUser = new User(process.env.RHD_BASE_URL).rhdAccountDetails();
             let login = new Login();
