8b61af7b2c0407a7eac17068ed99d77b53f1589e
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2018-11-20 18:17:03 +0000

    add db migration for published _at

---
+++ _docker/drupal/drupal-filesystem/db-migrations/20181120175207_published_at_migration.php
@@ -0,0 +1,18 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class PublishedAtMigration extends AbstractMigration
+{
+    /**
+     * Change Method.
+     * Copy published_date to published_at field, then copy created to published_at if its still null.
+     */
+    public function change()
+    {
+        $this->adapter->beginTransaction();
+            $this->execute("UPDATE drupal.lightning_node_field_data, drupal.lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(drupal.lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = drupal.lightning_node__field_published_date.entity_id AND vid = revision_id;");
+            $this->execute("UPDATE drupal.lightning_node_field_data SET published_at = created WHERE published_at IS NULL;");
+        $this->adapter->commitTransaction();
+    }
+}
