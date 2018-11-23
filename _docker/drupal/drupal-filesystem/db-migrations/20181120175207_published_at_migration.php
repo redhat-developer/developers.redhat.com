8d57846bc7f6b8f759a6f5f40d016fb4ea7a7410
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2018-11-23 09:40:00 +0000

    update migration sql

--- _docker/drupal/drupal-filesystem/db-migrations/20181120175207_published_at_migration.php
+++ _docker/drupal/drupal-filesystem/db-migrations/20181120175207_published_at_migration.php
@@ -12,7 +12,9 @@ class PublishedAtMigration extends AbstractMigration
     {
         $this->adapter->beginTransaction();
             $this->execute("UPDATE drupal.lightning_node_field_data, drupal.lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(drupal.lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = drupal.lightning_node__field_published_date.entity_id AND vid = revision_id;");
+            $this->execute("UPDATE drupal.lightning_node_field_revision, drupal.lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(drupal.lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = drupal.lightning_node__field_published_date.entity_id;");
             $this->execute("UPDATE drupal.lightning_node_field_data SET published_at = created WHERE published_at IS NULL;");
+            $this->execute("UPDATE drupal.lightning_node_field_revision SET published_at = created WHERE published_at IS NULL;");
         $this->adapter->commitTransaction();
     }
 }
