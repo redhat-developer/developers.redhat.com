28d3abe0c5891d1f60d60587efb6f6e5cccc29ec
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2018-11-26 18:27:22 +0000

    change startup order

--- setup_local_drupal.sh
+++ setup_local_drupal.sh
@@ -66,11 +66,6 @@ cd ${THEME}/rhd-frontend
 npm install && npm run-script build 
 cd ${PROJ}
 
-echo "Running DB Migrations"
-cd ${DRUPAL_FILESYSTEM}
-vendor/bin/phinx migrate
-cd ${PROJ}
-
 echo "Sanitizing the database"
 ${WEB}/../vendor/bin/drush --root=${WEB} sqlsan --sanitize-password=drupal --sanitize-email=user+%uid@example.com
 
@@ -92,6 +87,11 @@ ${WEB}/../vendor/bin/drush --root=${WEB} cr
 
 echo "Running drush cim"
 ${WEB}/../vendor/bin/drush --root=${WEB} -y cim
+
+echo "Running DB Migrations"
+cd ${DRUPAL_FILESYSTEM}
+vendor/bin/phinx migrate
+cd ${PROJ}
 
 echo "Starting the server"
 ${WEB}/../vendor/bin/drush --root=${WEB} rs
