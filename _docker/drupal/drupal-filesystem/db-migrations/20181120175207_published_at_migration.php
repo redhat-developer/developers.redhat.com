431634571fe1aef3c3a6acf84c31416bcd5a05c7
Alistair Mcgranaghan <alistair@alt-media.co.uk>
2018-11-29 10:15:05 +0000

    Remove bd name from script.

--- _docker/drupal/drupal-filesystem/db-migrations/20181120175207_published_at_migration.php
+++ _docker/drupal/drupal-filesystem/db-migrations/20181120175207_published_at_migration.php
@@ -11,10 +11,10 @@ class PublishedAtMigration extends AbstractMigration
     public function change()
     {
         $this->adapter->beginTransaction();
-            $this->execute("UPDATE drupal.lightning_node_field_data, drupal.lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(drupal.lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = drupal.lightning_node__field_published_date.entity_id AND vid = revision_id;");
-            $this->execute("UPDATE drupal.lightning_node_field_revision, drupal.lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(drupal.lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = drupal.lightning_node__field_published_date.entity_id;");
-            $this->execute("UPDATE drupal.lightning_node_field_data SET published_at = created WHERE published_at IS NULL;");
-            $this->execute("UPDATE drupal.lightning_node_field_revision SET published_at = created WHERE published_at IS NULL;");
+            $this->execute("UPDATE lightning_node_field_data, lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = lightning_node__field_published_date.entity_id AND vid = revision_id;");
+            $this->execute("UPDATE lightning_node_field_revision, lightning_node__field_published_date SET published_at = UNIX_TIMESTAMP(STR_TO_DATE(lightning_node__field_published_date.field_published_date_value, '%Y-%m-%d %h:%i%p')) WHERE nid = lightning_node__field_published_date.entity_id;");
+            $this->execute("UPDATE lightning_node_field_data SET published_at = created WHERE published_at IS NULL;");
+            $this->execute("UPDATE lightning_node_field_revision SET published_at = created WHERE published_at IS NULL;");
         $this->adapter->commitTransaction();
     }
 }
