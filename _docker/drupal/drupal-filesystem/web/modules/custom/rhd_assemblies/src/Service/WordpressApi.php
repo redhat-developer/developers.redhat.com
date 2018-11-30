3c6d31ad78ab7261b16202ceaace6f19a6e6eee0
Jordan White <jordan.page.white@gmail.com>
2018-11-30 15:17:05 -0800

    Add comments to rhd_assembly
    This adds method comments, and some other comments, to many of the
    services and controllers in rhd_assembly. It also implements some other
    code standard improvements such as whitespace, logger formatting,
    commas, etc.

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/src/Service/WordpressApi.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/src/Service/WordpressApi.php
@@ -6,19 +6,45 @@ use Drupal\Component\Utility\Html;
 use GuzzleHttp\ClientInterface;
 
 /**
- * Class WordpressApi
+ * Retrieves Wordpress content from an wp-json endpoint.
  */
 class WordpressApi implements RemoteContentApiInterface {
 
+  /**
+   * The base URL of the Wordpress blog.
+   *
+   * @var string
+   */
   private $apiUrl;
+
+  /**
+   * The Guzzle HTTP Client.
+   *
+   * @var GuzzleHttp\ClientInterface
+   */
   private $client;
 
+  /**
+   * {@inheritdoc}
+   */
   public function __construct(ClientInterface $client) {
-    //$this->apiUrl = 'https://developers.redhat.com/blog';
-    $this->apiUrl = 'https://origin-developers.redhat.com/blog'; // this url bypasses akamai
+    // This URL bypasses Akamai. If, for some reason, we do not want to bypass
+    // Akamai, change the value of this attribute to
+    // 'https://developers.redhat.com/blog'.
+    $this->apiUrl = 'https://origin-developers.redhat.com/blog';
     $this->client = $client;
   }
 
+  /**
+   * Fetches a WP post in JSON from a wp-json endpoint using the post ID.
+   *
+   * @param string $id
+   *   The ID of the WP post.
+   *
+   * @return object|false
+   *   Returns a stdClass of the WP post data if the fetch is successful.
+   *   Otherwise, this method return FALSE.
+   */
   public function getContentById($id) {
     try {
       $feed_url = $this->apiUrl . '/wp-json/wp/v2/posts/' . $id;
@@ -28,11 +54,28 @@ class WordpressApi implements RemoteContentApiInterface {
       return $this->getContentComposite($result);
     }
     catch (\Exception $e) {
-      \Drupal::logger('rhd_assemblies')->error("Exception while calling Wordpress API: " . $e->getMessage());
-      return false;
+      \Drupal::logger('rhd_assemblies')->error(
+        "Exception while calling Wordpress API: @message", [
+          '@message' => $e->getMessage(),
+        ]
+      );
+
+      return FALSE;
     }
   }
 
+  /**
+   * Fetches WP post(s) by their category. Returns as many as $count posts.
+   *
+   * @param array $categories
+   *   An array of strings of the WP post categories.
+   * @param string|int $count
+   *   Maximum number of items to be returned in result set.
+   *
+   * @return array
+   *   Returns an array of objects of the WP posts if the fetch is successful.
+   *   Otherwise, returns an empty array.
+   */
   public function getContentByCategory($categories, $count) {
     $items = [];
     $feed_url = $this->apiUrl . '/wp-json/wp/v2/posts';
@@ -43,49 +86,93 @@ class WordpressApi implements RemoteContentApiInterface {
     }
 
     try {
+      // Retrieve the WP posts from the $feed_url and decode the JSON into a
+      // $results array.
       $request = $this->client->request('GET', $feed_url, ['query' => $query]);
       $response = $request->getBody()->getContents();
       $results = json_decode($response);
+
+      // Iterate of the WP results returned in $results, and get the
+      // processed/formatted results from getContentComposite().
       foreach ($results as $result) {
         $items[] = $this->getContentComposite($result);
       }
+
       return $items;
     }
     catch (\Exception $e) {
-      \Drupal::logger('rhd_assemblies')->error("Exception while calling Wordpress API: " . $e->getMessage());
+      \Drupal::logger('rhd_assemblies')->error(
+        "Exception while calling Wordpress API: @message", [
+          '@message' => $e->getMessage(),
+        ]
+      );
+
       return [];
     }
   }
 
+  /**
+   * Returns $max_results-many autocomplete options based upon a $search value.
+   *
+   * @param string $search
+   *   A search query to use to filter WP posts by title.
+   * @param int $max_results
+   *   The maximum number of results to provide in the autocomplete options.
+   *
+   * @return array
+   *   Return an array of autocomplete options if posts can be successfully
+   *   fetched. Otherwise, returns an empty array.
+   */
   public function getAutocompleteContentOptions($search, $max_results) {
     $feed_url = $this->apiUrl;
     $query = [
       'rest_route' => '/rh/v1/posts-by-title',
-      'search' => $search
+      'search' => $search,
     ];
     $results = [];
+
     try {
+      // Tries to retrieve WP posts from $feed_url using the $query params.
       $request = $this->client->request('GET', $feed_url, ['query' => $query]);
       $response = $request->getBody()->getContents();
       $api_results = json_decode($response);
+
       foreach ($api_results as $api_result) {
         $label = Html::escape($api_result->post_title) . ' (' . $api_result->ID . ')';
         $results[] = [
           'value' => $label,
-          'label' => $label
+          'label' => $label,
         ];
+
         if (count($results) >= $max_results) {
+          // If we already have $max_results-many WP posts in $results, then we
+          // return $results.
           return $results;
         }
       }
+
+      // If no WP posts were returned in the response, return the empty $results
+      // array here.
       return $results;
     }
     catch (\Exception $e) {
-      \Drupal::logger('rhd_assemblies')->error("Exception while calling Wordpress API: " . $e->getMessage());
+      \Drupal::logger('rhd_assemblies')->error(
+        "Exception while calling Wordpress API: @message", [
+          '@message' => $e->getMessage(),
+        ]
+      );
+
       return [];
     }
   }
 
+  /**
+   * Fetches WP post categories from the wp-json endpoint.
+   *
+   * @return array
+   *   Returns an array of categories and metadata if the data is successfully
+   *   fetched. Otherwise, returns an empty array.
+   */
   public function getCategories() {
     $feed_url = $this->apiUrl . '/wp-json/wp/v2/categories';
     $query = [
@@ -94,6 +181,7 @@ class WordpressApi implements RemoteContentApiInterface {
     ];
     $pages_to_query = 1;
     $results = [];
+
     try {
       for ($i = 1; $i <= $pages_to_query; $i++) {
         $query['page'] = $i;
@@ -102,31 +190,40 @@ class WordpressApi implements RemoteContentApiInterface {
         $pages_to_query = reset($request_headers['X-WP-TotalPages']);
         $response = $request->getBody()->getContents();
         $api_results = json_decode($response);
+
         foreach ($api_results as $api_result) {
           $results[$api_result->id] = $api_result;
         }
       }
+
       return $results;
     }
     catch (\Exception $e) {
-      \Drupal::logger('rhd_assemblies')->error("Exception while calling Wordpress API: " . $e->getMessage());
+      \Drupal::logger('rhd_assemblies')->error(
+        "Exception while calling Wordpress API: @message", [
+          '@message' => $e->getMessage(),
+        ]
+      );
+
       return [];
     }
   }
 
   public function getCategoryOptions() {
     $categories = $this->getCategories();
+
     foreach ($categories as $id => $category) {
       $categories[$id] = $category->name;
     }
+
     return $categories;
   }
 
   private function getContentComposite($content) {
     $item = new \stdClass();
     $item->content = $content;
-    $item->media = false;
-    $item->categories = false;
+    $item->media = FALSE;
+    $item->categories = FALSE;
     $item->date = date('F j, Y', strtotime($content->date));
 
     if (isset($content->featured_media) && $content->featured_media) {
@@ -142,30 +239,57 @@ class WordpressApi implements RemoteContentApiInterface {
     return $item;
   }
 
+  /**
+   * Retrieves WP media by the $id of the media.
+   *
+   * @param string $id
+   *   The ID of the WP media.
+   *
+   * @return array
+   *   Returns an array of metadata for the media entity if successfully
+   *   fetched. Otherwise, returns an empty array.
+   */
   private function getContentMedia($id) {
     try {
       $feed_url = $this->apiUrl . '/wp-json/wp/v2/media/' . $id;
       $request = $this->client->request('GET', $feed_url);
       $response = $request->getBody()->getContents();
+
       return json_decode($response);
     }
     catch (\Exception $e) {
-      \Drupal::logger('rhd_assemblies')->error("Exception while calling Wordpress API: " . $e->getMessage());
+      \Drupal::logger('rhd_assemblies')->error(
+        "Exception while calling Wordpress API: @message", [
+          '@message' => $e->getMessage(),
+        ]
+      );
+
       return [];
     }
   }
 
+  /**
+   * Retrieves categories from WP, then extracts links and names.
+   *
+   * @param array $categories
+   *   An array of category IDs.
+   *
+   * @return array
+   *   If successful, this returns an array of category links and names indexed
+   *   by the category ID. Otherwise, this returns an empty array.
+   */
   private function getContentCategoryLinks($categories) {
     $links = [];
     $categories_list = $this->getCategories();
-    foreach($categories as $category_id) {
+
+    foreach ($categories as $category_id) {
       $links[] = [
         'link' => $categories_list[$category_id]->link,
         'name' => $categories_list[$category_id]->name,
       ];
     }
+
     return $links;
   }
-
 
 }
