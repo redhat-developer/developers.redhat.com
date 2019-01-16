25fe84c78e45989638b6c32e377fd6d8ecf4a6e1
Ian Hamilton <ian.hamilton@dnata.com>
2019-01-16 12:37:17 +0000

    Updating tests due to sso changes.

--- _tests/e2e/specs/support/pages/keycloak/Login.page.js
+++ _tests/e2e/specs/support/pages/keycloak/Login.page.js
@@ -11,13 +11,19 @@ export class Login extends Page {
         this.addSelectors({
             loginPage: '.kc-loginpage',
             usernameField: '#username',
+            nextBtn: '#login-show-step2',
             passwordField: '#password',
-            loginButton: '#kc-login'
+            loginButton: '#kc-login',
+            step2: '#step2'
         });
     }
 
     awaitLogin() {
         return this.awaitExists(this.getSelector('loginPage'), 30000) && this.waitForPageTitle('Log In', 30000);
+    }
+
+    awaitStepTwo() {
+        return this.awaitIsVisible(this.getSelector('step2'), 30000);
     }
 
     isDisplayed() {
@@ -28,6 +34,8 @@ export class Login extends Page {
     with(user) {
         this.awaitLogin();
         this.type(user['email'], this.getSelector('usernameField'));
+        this.click(this.getSelector('nextBtn'));
+        this.awaitStepTwo();
         this.type(user['password'], this.getSelector('passwordField'));
         this.click(this.getSelector('loginButton'));
         return this.awaitIsLoggedIn(user)
