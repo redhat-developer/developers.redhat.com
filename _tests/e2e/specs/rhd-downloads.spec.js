ae557b2eb02857d65142f278f7e636ab62145369
Ian Hamilton <ian.hamilton@dnata.com>
2019-03-01 09:38:13 +0400

    ignore failing tests

--- _tests/e2e/specs/rhd-downloads.spec.js
+++ _tests/e2e/specs/rhd-downloads.spec.js
@@ -1,62 +1,62 @@
-// import {Login} from './support/pages/Login.page';
-// import {ProductOverview} from './support/pages/ProductOverview.page';
-// import {CheatSheets} from './support/pages/CheatSheets.page';
-// import {User} from './support/rest/keycloak/Site.user';
-// import {DownloadDir} from './support/DownloadDir';
-// import {Utils} from './support/Utils';
-//
-// const tags = require('mocha-tags');
-//
-// tags('desktop').describe('Download Manager', function () {
-//     this.retries(2);
-//
-//     beforeEach(function () {
-//         let downloadDir = new DownloadDir();
-//         downloadDir.clear(global.downloadDir);
-//         new Utils().cleanSession();
-//     }, 2);
-//
-//     tags('dm')
-//         .it('should allow users to login in and download RHEL', function () {
-//             let downloadDir = new DownloadDir();
-//             let siteUser = new User(process.env.RHD_BASE_URL).rhdAccountDetails();
-//             let loginPage = new Login();
-//             let productOverview = new ProductOverview('rhel', 'download', 'Red Hat Enterprise Linux');
-//             productOverview
-//                 .open();
-//             productOverview
-//                 .download();
-//             loginPage.keycloak
-//                 .loginWith(siteUser);
-//             productOverview
-//                 .awaitDownload();
-//             let downloadName = downloadDir.get();
-//             expect(downloadName.toString(), 'rhel download was not triggered').to.include('rhel');
-//         });
-//
-//     tags('dm')
-//         .it('should allow users to log-in and download advanced-linux-commands', function () {
-//             let downloadDir = new DownloadDir();
-//             let siteUser = new User(process.env.RHD_BASE_URL).rhdAccountDetails();
-//             let loginPage = new Login();
-//             let cheatSheet = new CheatSheets('advanced-linux-commands');
-//             cheatSheet
-//                 .open();
-//             cheatSheet
-//                 .awaitLoaded();
-//             cheatSheet
-//                 .loginToDownload();
-//             loginPage.keycloak
-//                 .loginWith(siteUser);
-//             cheatSheet
-//                 .awaitDownloadThankYou();
-//             let downloadName = downloadDir.get();
-//             expect(downloadName.toString(), 'rhel advanced linux cheatsheet download was not triggered').to.include('rheladvancedlinux_cheat_sheet')
-//         });
-//
-//     afterEach(function () {
-//         let downloadDir = new DownloadDir();
-//         downloadDir.clear(global.downloadDir);
-//         new Utils().cleanSession();
-//     }, 2);
-// });
+import {Login} from './support/pages/Login.page';
+import {ProductOverview} from './support/pages/ProductOverview.page';
+import {CheatSheets} from './support/pages/CheatSheets.page';
+import {User} from './support/rest/keycloak/Site.user';
+import {DownloadDir} from './support/DownloadDir';
+import {Utils} from './support/Utils';
+
+const tags = require('mocha-tags');
+
+tags('desktop').describe('Download Manager', function () {
+    this.retries(2);
+
+    beforeEach(function () {
+        let downloadDir = new DownloadDir();
+        downloadDir.clear(global.downloadDir);
+        new Utils().cleanSession();
+    }, 2);
+
+    tags('dm')
+        .it('should allow users to login in and download RHEL', function () {
+            let downloadDir = new DownloadDir();
+            let siteUser = new User(process.env.RHD_BASE_URL).rhdAccountDetails();
+            let loginPage = new Login();
+            let productOverview = new ProductOverview('rhel', 'download', 'Red Hat Enterprise Linux');
+            productOverview
+                .open();
+            productOverview
+                .download();
+            loginPage.keycloak
+                .loginWith(siteUser);
+            productOverview
+                .awaitDownload();
+            let downloadName = downloadDir.get();
+            expect(downloadName.toString(), 'rhel download was not triggered').to.include('rhel');
+        });
+
+    tags('dm')
+        .it('should allow users to log-in and download advanced-linux-commands', function () {
+            let downloadDir = new DownloadDir();
+            let siteUser = new User(process.env.RHD_BASE_URL).rhdAccountDetails();
+            let loginPage = new Login();
+            let cheatSheet = new CheatSheets('advanced-linux-commands');
+            cheatSheet
+                .open();
+            cheatSheet
+                .awaitLoaded();
+            cheatSheet
+                .loginToDownload();
+            loginPage.keycloak
+                .loginWith(siteUser);
+            cheatSheet
+                .awaitDownloadThankYou();
+            let downloadName = downloadDir.get();
+            expect(downloadName.toString(), 'rhel advanced linux cheatsheet download was not triggered').to.include('rheladvancedlinux_cheat_sheet')
+        });
+
+    afterEach(function () {
+        let downloadDir = new DownloadDir();
+        downloadDir.clear(global.downloadDir);
+        new Utils().cleanSession();
+    }, 2);
+});
