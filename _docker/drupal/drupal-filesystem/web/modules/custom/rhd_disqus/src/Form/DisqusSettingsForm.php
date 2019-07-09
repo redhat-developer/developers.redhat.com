20e69adfe734800410fb970b7456f4fd2f281a32
Alistair McGranaghan <alistair@alt-media.co.uk>
2019-07-09 15:13:20 +0100

    remove all but shortname from the form

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_disqus/src/Form/DisqusSettingsForm.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_disqus/src/Form/DisqusSettingsForm.php
@@ -30,24 +30,7 @@ class DisqusSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    //$config = $this->config('rhd_disqus.disqussettings');
     $config = \Drupal::config('rhd_disqus.disqussettings');
-    $form['rhd_disqus_api_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Disqus API Key'),
-      '#attributes' => array('readonly' => 'readonly'),
-      '#maxlength' => 64,
-      '#size' => 64,
-      '#default_value' => $config->get('rhd_disqus_api_key'),
-    ];
-    $form['rhd_disqus_secret_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Disqus API Secret'),
-      '#attributes' => array('readonly' => 'readonly'),
-      '#maxlength' => 64,
-      '#size' => 64,
-      '#default_value' => $config->get('rhd_disqus_secret_key'),
-    ];
     $form['rhd_disqus_shortname'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Disqus Shortname'),
@@ -55,18 +38,6 @@ class DisqusSettingsForm extends ConfigFormBase {
       '#maxlength' => 64,
       '#size' => 64,
       '#default_value' => $config->get('rhd_disqus_shortname'),
-    ];
-    $form['rhd_disqus_email_author_enabled'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Author gets email on comment enabled'),
-      '#attributes' => array('disabled' => 'disabled'),
-      '#default_value' => $config->get('rhd_disqus_email_author_enabled'),
-    ];
-    $form['rhd_disqus_email_author_debug'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Debug mode enabled'),
-      '#attributes' => array('disabled' => 'disabled'),
-      '#default_value' => $config->get('rhd_disqus_email_author_debug'),
     ];
     return parent::buildForm($form, $form_state);
   }
