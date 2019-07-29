f996ee9bfb9516b97fb5731b3a0ec7b64d0d7ac0
robpblake <rob@rb-associates.co.uk>
2019-07-29 16:25:52 +0100

    [MWES-2871] First steps at getting Drupal to run with memcached as a cache rather than the database

--- _docker/drupal/dev/run-drupal.sh
+++ _docker/drupal/dev/run-drupal.sh
@@ -2,15 +2,16 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 docker pull docker-registry.engineering.redhat.com/developers/drupal-data:latest
-docker pull registry.paas.redhat.com/rhdp/developer-base
-cd $DIR && docker-compose down -v
-cd $DIR && rm -rf $DIR/drupal-workspace
-cd $DIR && docker-compose build
-cd $DIR && mkcert localhost
-cd $DIR && mkdir -p drupal-workspace
-cd $DIR && docker-compose run --rm seed_env
-cd $DIR && docker-compose run --rm bootstrap_env
-cd $DIR && docker-compose run --rm bootstrap_drupal
-cd $DIR && chmod -R 777 ./drupal-workspace/drupal_1/drupal/sites/default/files
-cd $DIR && docker-compose up -d drupal
-cd $DIR && docker-compose logs -f drupal
+docker pull images.paas.redhat.com/rhdp/developer-base:rhel-76.1
+cd ${DIR} && docker-compose down -v
+cd ${DIR} && rm -rf $DIR/drupal-workspace
+cd ${DIR} && docker-compose build
+cd ${DIR} && docker-compose up -d mysql memcached memcached2
+cd ${DIR} && mkcert localhost
+cd ${DIR} && mkdir -p drupal-workspace
+cd ${DIR} && docker-compose run --rm seed_env
+cd ${DIR} && docker-compose run --rm bootstrap_env
+cd ${DIR} && docker-compose run --rm bootstrap_drupal
+cd ${DIR} && chmod -R 777 ./drupal-workspace/drupal_1/drupal/sites/default/files
+cd ${DIR} && docker-compose up -d drupal
+cd ${DIR} && docker-compose logs -f drupal
