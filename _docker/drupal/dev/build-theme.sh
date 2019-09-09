c92ac4e0de69fe85d113a5963138b7bd806ea11b
robpblake <rob@rb-associates.co.uk>
2019-09-09 18:52:43 +0100

    [No Issue] Support regenerating the rhd-frontend assets as part of the local Drupal development experience

---
+++ _docker/drupal/dev/build-theme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+FE_THEME_DIR="${DIR}/../drupal-filesystem/web/themes/custom/rhdp2/rhd-frontend"
+DRUPAL_THEME_DIR="${DIR}/../drupal-filesystem/web/themes/custom/rhdp2"
+
+mkdir -p "${DRUPAL_THEME_DIR}/css"
+mkdir -p "${DRUPAL_THEME_DIR}/js"
+
+set -a
+source "${DIR}"/local-config.sh
+set +a
+
+cd "${FE_THEME_DIR}" \
+&& npm config set "registry" https://repository.engineering.redhat.com/nexus/repository/registry.npmjs.org/ \
+&& npm config set strict-ssl false \
+&& npm config set "@fortawesome:registry" https://npm.fontawesome.com/ \
+&& npm config set "//npm.fontawesome.com/:_authToken" $FONTAWESOME_LICENCE \
+&& npm install \
+&& npm run-script styles \
+&& npm run-script build \
+&& cp dist/css-min/rhd.min.css "${DRUPAL_THEME_DIR}"/css/ \
+&& cp dist/css-min/rhd.microsites.css "${DRUPAL_THEME_DIR}"/css/ \
+&& cp dist/js/@rhd/rhd.old.min.js "${DRUPAL_THEME_DIR}"/js/ \
+&& cp -r favicons "${DRUPAL_THEME_DIR}/"
