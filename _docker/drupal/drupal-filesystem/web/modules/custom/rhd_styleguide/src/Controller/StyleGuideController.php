b941a9342aadafb4e151df530af6b52b3190a90c
Matt Moen <matt@alloymagnetic.com>
2019-09-24 10:10:50 -0600

    Add more elements to styleguide

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_styleguide/src/Controller/StyleGuideController.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_styleguide/src/Controller/StyleGuideController.php
@@ -4,10 +4,14 @@
  * Contains \Drupal\rhd_styleguide\Controller\StyleGuideController.
  */
 namespace Drupal\rhd_styleguide\Controller;
+
 class StyleGuideController {
   public function content() {
     return [
-        '#theme' => 'rhd_styleguide',
+      '#theme' => 'rhd_styleguide',
+      '#form' => \Drupal::formBuilder()->getForm('Drupal\rhd_styleguide\Form\DemoForm'),
     ];
+
   }
+
 }
