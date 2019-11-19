61e26f6dd8620c1383adc821301a186d15de2b95
Derek <derek@alloymagnetic.com>
2019-11-19 17:29:12 -0500

    initial import of authors and posts

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_migration/src/Plugin/migrate/source/WordpressAuthor.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_migration/src/Plugin/migrate/source/WordpressAuthor.php
@@ -4,38 +4,76 @@ namespace Drupal\rhd_migration\Plugin\migrate\source;
 
 use Drupal\Core\Database\Database;
 use Drupal\migrate\Row;
+use Drupal\migrate\Plugin\MigrationInterface\MigrateExecutableInterface;
 
 /**
  * Extract authors
  * 
  * @MigrateSource(
- *   id = "wordpress_author"
+ *   id = "wordpress_author",
+ *   source_module = "rhd_migration"
  * )
  */
 class WordpressAuthor extends WpSqlBase {
 
   public function query() {
-    // Database::setActiveConnection('wp');
     $db = Database::getConnection('default', 'wp');
-    $query = $db->select('wp_users', 'users')
-      ->fields('users', ['ID', 'user_login', 'user_email']);
-    // Database::setActiveConnection();
+    $query = $db->select('wp_users', 'users');
+    $query->addField('users', 'ID');
+    $query->addField('users', 'user_email', 'author_email');
+    $query->addField('users', 'user_login', 'author_login');
+
+    $meta = [
+      'author_title' => 'user_job_title',
+      'author_bio' => 'description',
+      'author_twitter' => 'user_twitter_handle',
+      'author_youtube' => 'youtube',
+      'author_linkedin' => 'user_linkedin_profile',
+      'author_github' => 'user_github_profile',
+      'author_facebook' => 'user_facebook_profile',
+      'author_last_name' => 'last_name',
+      'author_first_name' => 'first_name',
+    ];
+    foreach ($meta as $field => $meta_key) {
+      $join_condition = 'users.ID = ' . $field . '.user_id and ' . $field . '.meta_key = \'' . $meta_key . '\'';
+      $query->leftJoin('wp_usermeta', $field, $join_condition);
+      $query->addField($field, 'meta_value', $field);
+    }
+
     return $query;
   }
 
   public function fields() {
     return [
+      'author_id' => $this->t('Author ID'),
       'author_login' => $this->t('Author Login'),
+      'author_url_alias' => $this->t('Author URL Alias'),
       'author_email' => $this->t('Author Email'),
       'author_display_name' => $this->t('Author Display Name'),
       'author_first_name' => $this->t('Author First Name'),
-      'author_last_name' => $this->t('Author Last Name')
+      'author_last_name' => $this->t('Author Last Name'),
+      'author_bio' => $this->t('Author Bio'),
+      'author_title' => $this->t('Author Title'),
+      'author_linkedin' => $this->t('Author Linkedin'),
+      'author_twitter' => $this->t('Author Twitter'),
+      'author_facebook' => $this->t('Author Facebook'),
+      'author_stackoverflow' => $this->t('Author StackOverflow'),
+      'author_youtube' => $this->t('Author YouTube'),
+      'author_url' => $this->t('Author URL'),
     ];
+  }
+
+  public function prepareRow(Row $row) {
+    $fname = $row->getSourceProperty('author_first_name');
+    $lname = $row->getSourceProperty('author_last_name');
+    $display_name = implode(' ', [$fname, $lname]);
+    $row->setSourceProperty('author_display_name', $display_name);
+    $row->setSourceProperty('author_url_alias', '/blog/author/' . $row->getSourceProperty('author_login'));
   }
 
   public function getIds() {
     return [
-      'user_id' => [
+      'ID' => [
         'type' => 'integer'
       ]
     ];
--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_migration/src/Plugin/migrate/source/WordpressPost.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_migration/src/Plugin/migrate/source/WordpressPost.php
@@ -6,32 +6,77 @@ use Drupal\Core\Database\Database;
 use Drupal\migrate\Row;
 
 /**
- * Extract authors
+ * Extract posts
  * 
  * @MigrateSource(
- *   id = "wordpress_post"
+ *   id = "wordpress_post",
+ *   source_module = "rhd_migration"
  * )
  */
 class WordpressPost extends WpSqlBase {
 
   public function query() {
     $db = Database::getConnection('default', 'wp');
-    $query = $db->select('wp_posts', 'posts')
-      ->fields('posts', ['ID', 'post_title', 'post_author'])
-      ->condition('posts.post_type', 'post');
+    $query = $db->select('wp_posts', 'posts');
+    $query->condition('posts.post_type', 'post');
+    $query->condition('posts.post_title', '', '!=');
+    $query->addField('posts', 'ID');
+    $query->addField('posts', 'post_author');
+    $query->addField('posts', 'post_date', 'post_created');
+    $query->addField('posts', 'post_modified', 'post_changed');
+    $query->addField('posts', 'post_status');
+    $query->addField('posts', 'post_name', 'post_url_fragment');
+    $query->addField('posts', 'post_content');
+    $query->addField('posts', 'post_title');
     return $query;
   }
 
   public function fields() {
     return [
-      'title' => $this->t('Title'),
-      'author' => $this->t('Author'),
+      'post_id' => $this->t('Post ID'),
+      'post_author' => $this->t('Post Author'),
+      'post_created' => $this->t('Created'),
+      'post_changed' => $this->t('Changed'),
+      'post_status' => $this->t('Status'),
+      'post_content' => $this->t('Content'),
+      'post_title' => $this->t('Title'),
+      'post_url' => $this->t('URL'),
     ];
+  }
+
+  public function prepareRow(Row $row) {
+    $url[] = '/blog';
+    $url[] = date('Y', strtotime($row->getSourceProperty('post_created')));
+    $url[] = date('m', strtotime($row->getSourceProperty('post_created')));
+    $url[] = date('d', strtotime($row->getSourceProperty('post_created')));
+    $url[] = $row->getSourceProperty('post_url_fragment');
+    $row->setSourceProperty('post_url', implode('/', $url));
+
+    $status = $row->getSourceProperty('post_status');
+    switch ($status) {
+      case 'publish':
+        $row->setSourceProperty('post_status', 'published');
+        break;
+      case 'pending-review':
+      case 'pending':
+      case 'future':
+        $row->setSourceProperty('post_status', 'needs_review');
+        break;
+      case 'auto-draft':
+      case 'trash':
+      case 'private':
+        $row->setSourceProperty('post_status', 'archived');
+        break;
+      default:
+        $row->setSourceProperty('post_status', 'draft');
+        break;
+    }
+    kint($row);
   }
 
   public function getIds() {
     return [
-      'post_id' => [
+      'ID' => [
         'type' => 'integer'
       ]
     ];
