2b50a6b6b90dc722ba3c0f179a6ccb070185d5bd
Stacey Mosier <mosierdesign@gmail.com>
2019-12-19 16:44:25 -0500

    Fix js adding hljs class twice to pre tags

--- _docker/drupal/drupal-filesystem/web/themes/custom/rhdp2/rhd-frontend/src/scripts/js/rhd/1.2-highlight-init.js
+++ _docker/drupal/drupal-filesystem/web/themes/custom/rhdp2/rhd-frontend/src/scripts/js/rhd/1.2-highlight-init.js
@@ -1,8 +1,4 @@
 (function() {
-  /* Init Highlightjs */
+  // Init Highlightjs.
   hljs.initHighlightingOnLoad();
-
-  $('.assembly-type-code_snippet pre code, #rhd-article .field--name-body .field__item > pre').each(function (i, block) {
-    hljs.highlightBlock(block);
-  });
 })();
