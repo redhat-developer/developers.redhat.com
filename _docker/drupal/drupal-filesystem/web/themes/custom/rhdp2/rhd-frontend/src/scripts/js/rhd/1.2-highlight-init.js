4fac55b9507c72cc11a06d100f630d6061ad764c
Stacey Mosier <mosierdesign@gmail.com>
2019-12-19 08:42:09 -0500

    Move highlightjs to rhd-frontend, wip

--- _docker/drupal/drupal-filesystem/web/themes/custom/rhdp2/rhd-frontend/src/scripts/js/rhd/1.2-highlight-init.js
+++ _docker/drupal/drupal-filesystem/web/themes/custom/rhdp2/rhd-frontend/src/scripts/js/rhd/1.2-highlight-init.js
@@ -1,5 +1,8 @@
-(function() { 
+(function() {
   /* Init Highlightjs */
   hljs.initHighlightingOnLoad();
-})();
 
+  $('.assembly-type-code_snippet pre code, #rhd-article .field--name-body .field__item > pre').each(function (i, block) {
+    hljs.highlightBlock(block);
+  });
+})();
