104803d553244480fe69d59f28803900da9ef505
Jordan White <jordan.page.white@gmail.com>
2020-01-30 09:57:53 -0800

    RHDX-131: Add CSS and JS sourcemaps
    This adds CSS and JS sourcemaps to our rhdp2/rhd-frontend assets/build
    process. This required removing some build logic that was moving dist
    files around in Dockerfile.mp and dev/build-rhdp2-theme.sh, updating
    paths across various FE build and config files and of course adding
    to the existing node-sass command(s) in package.json to include the
    building of sourcemaps.

--- _docker/drupal/dev/build-rhdp2-theme.sh
+++ _docker/drupal/dev/build-rhdp2-theme.sh
@@ -15,15 +15,6 @@ cd "${FE_THEME_DIR}" \
 && npm config set "registry" https://repository.engineering.redhat.com/nexus/repository/registry.npmjs.org/ \
 && npm install \
 && npm run build \
-&& cp dist/css-min/rhd.min.css "${DRUPAL_THEME_DIR}"/css/ \
-&& cp dist/css-min/rhd.microsites.css "${DRUPAL_THEME_DIR}"/css/ \
-&& cp dist/css-min/rhd.legacy.css "${DRUPAL_THEME_DIR}"/css/ \
-&& cp dist/js/@rhd/rhd.old.min.js "${DRUPAL_THEME_DIR}"/js/ \
-&& cp dist/js/@rhd/rhd.min.js "${DRUPAL_THEME_DIR}"/js/ \
-&& cp dist/js/@patternfly/pfelement/pfelement.umd.min.js "${DRUPAL_THEME_DIR}"/js/ \
-&& cp dist/js/@patternfly/pfe-datetime/pfe-datetime.umd.min.js "${DRUPAL_THEME_DIR}"/js/ \
-&& rm -rf "${DRUPAL_THEME_DIR}"/js/rhd \
-&& cp -r dist/js/@rhd "${DRUPAL_THEME_DIR}"/js/rhd \
 && cp -r favicons "${DRUPAL_THEME_DIR}/" \
 && cp -r dist/fonts "${DRUPAL_THEME_DIR}/"
 
