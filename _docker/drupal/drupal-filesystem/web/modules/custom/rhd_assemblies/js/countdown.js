1fe4818ba581df5f78b6e533d890a409ebfa12e5
Stacey Mosier <mosierdesign@gmail.com>
2020-02-06 17:46:48 -0500

    Update js to update timer

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/js/countdown.js
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/js/countdown.js
@@ -7,30 +7,6 @@
 
 (function ($, Drupal) {
 
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    var daysSpan = clock.querySelector('.days');
-    var hoursSpan = clock.querySelector('.hours');
-    var minutesSpan = clock.querySelector('.minutes');
-    var secondsSpan = clock.querySelector('.seconds');
-
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-
-      daysSpan.innerHTML = t.days;
-      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-      if (t.total <= 0) {
-        clearInterval(timeinterval);
-      }
-    }
-
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-
   function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
     var seconds = Math.floor((t / 1000) % 60);
@@ -46,20 +22,35 @@
     };
   }
 
+  function initializeClock(clock, endtime) {
+    var daysSpan = clock.find('.days span');
+    var hoursSpan = clock.find('.hours span');
+    var minutesSpan = clock.find('.minutes span');
+    var secondsSpan = clock.find('.seconds span');
+
+    function updateClock() {
+      var t = getTimeRemaining(endtime);
+      daysSpan.html(t.days);
+      hoursSpan.html(('0' + t.hours).slice(-2));
+      minutesSpan.html(('0' + t.minutes).slice(-2));
+      secondsSpan.html(('0' + t.seconds).slice(-2));
+
+      if (t.total <= 0) {
+        clearInterval(timeinterval);
+        $('.cta__cta', clock).removeClass('hide');
+        $(clock).detach();
+      }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+  }
 
   Drupal.behaviors.rhd_countdown = {
     attach: function (context, settings) {
       $('.assembly-type-call_to_action.has-countdown', context).once('rhdCallToActionCountdown').each(function (){
-        console.log(settings.rhd_assemblies);
-
-
-        // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-        // initializeClock('clockdiv', deadline);
-
-
-        // Show the call to action.
-        // $('.cta__cta', $(this)).removeClass('hide');
-
+        var deadline = new Date(Date.parse(settings.rhd_assemblies.countdown_date));
+        initializeClock( $('.rhd-c-countdown', $(this)), deadline);
       });
     }
   }
