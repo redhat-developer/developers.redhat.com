b5935049b612fe35f990837cd3c1543cb23d9b3a
Jordan White <jordan.page.white@gmail.com>
2020-02-18 11:50:13 -0800

    Prevent Cheatsheets test from running on Pre-prod
    This adds back in a conditional that prevents the cheatsheet test from
    being executed in Pre-prod environments. I have created RHDX-300 to fix
    issues that prevent us from successfully executing the cheatsheets test.
    Please review that JIRA issue for context.

--- _tests/e2e/tests/specs/export/rhd-downloads.spec.js
+++ _tests/e2e/tests/specs/export/rhd-downloads.spec.js
@@ -23,14 +23,26 @@ tags('desktop').describe('Download Manager', function() {
         expect(downloadName.toString(), 'rhel download was not triggered').to.include('rhel');
     });
 
-    it('@downloads: should allow users to log-in and download advanced-linux-commands', function() {
-        CheatSheets
-            .open('advanced-linux-commands')
-            .loginToDownload();
-        Login.with(process.env.RHD_KEYCLOAK_ADMIN_USERNAME, process.env.RHD_KEYCLOAK_ADMIN_PASSWORD);
-        CheatSheets.awaitDownloadThankYou();
-        const downloadName = DownloadDir.get();
-        expect(downloadName.toString(), 'rhel advanced linux cheatsheet download was not triggered').to.include('rheladvancedlinux_cheat_sheet');
+    it('@downloads: should allow users to login and download advanced-linux-commands', function() {
+        /*
+            Please see https://issues.jboss.org/browse/DEVELOPER-5938
+            The DownloadManager is hard-coded and does not respect environment configuration. This means that some Downloads are hard-coded to use production URLs. We cannot
+            test these in lower environments because the authentication flow is not correct (non-PR environments do not authenticate with production Keycloak).
+            Fundamentally DownloadManager should respect environment configuration.
+
+						This has become more complicated unfortunately because a number of Cheatsheets and Ebooks now are created using the Page content type. The download manager links on
+						these pages are entered into WYSIWYG fields of Rich Text assemblies. This makes it much more difficult to target and contextually switch the download manager link(s)
+            to the correct environment.
+         */
+        if (Utils.isProduction()) {
+          CheatSheets
+              .open('advanced-linux-commands')
+              .loginToDownload();
+          Login.with(process.env.RHD_KEYCLOAK_ADMIN_USERNAME, process.env.RHD_KEYCLOAK_ADMIN_PASSWORD);
+          CheatSheets.awaitDownloadThankYou();
+          const downloadName = DownloadDir.get();
+          expect(downloadName.toString(), 'rhel advanced linux cheatsheet download was not triggered').to.include('rheladvancedlinux_cheat_sheet');
+        }
     });
 
     afterEach(function() {
