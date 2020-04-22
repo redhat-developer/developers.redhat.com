73c992957372296eb6558ebef583feb912a0ee02
Roger Sullivan <roger@alloymagnetic.com>
2020-04-22 10:18:35 -0600

    events_collection can get sessions from a single event.

--- _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/src/Plugin/AssemblyBuild/EventsCollectionBuild.php
+++ _docker/drupal/drupal-filesystem/web/modules/custom/rhd_assemblies/src/Plugin/AssemblyBuild/EventsCollectionBuild.php
@@ -14,75 +14,49 @@ use Drupal\assembly\Plugin\AssemblyBuildView;
  * )
  */
 class EventsCollectionBuild extends AssemblyBuildView {
-  /*
-   * @TODO get field_display_option to either choose display
-   * or set arguments/prepare args.
-   * @TODO get sessions from field_single_event_reference if
-   * field_display_option is set to single_event_sessions.
-   */
 
   /**
-   * Return a list of views that should get added to this view builder.
-   *
-   * Return value is an associative array structured like so:
-   * return [
-   *   'some_key' => [
-   *     'view' => 'some_view_id',
-   *     'display' => 'some_display_id' // optional, will use "default"
-   *     'weight' => 99, // an optional weight value. defaults to 99
-   *   ]
-   * ];
-   *
-   * @return array
-   *   The list of views
+   * {@inheritdoc}
    */
   protected function views() {
-    return ['events_collection' => ['view' => 'events_collection', 'display' => 'default']];
+    $views = [
+      'events_collection_grouped' => [
+        'view' => 'events_collection',
+        'display' => 'all_sessions_ungrouped',
+      ],
+    ];
+
+    return $views;
   }
 
   /**
-   * An associative array of field names to argument mapping settings.
-   *
-   * Takes the values provided by fields in the assembly and feeds them
-   * to the view as contextual filters (arguments).
-   *
-   * By way of example:
-   *  return [
-   *    'field_categories' => [
-   *      'index' => 0, // the argument index to which it. Required
-   *      'view' => 'some_view_id', // The id of the view to which
-   *                                // this gets assigned.
-   *                                // Optional if only one view is assigned
-   *      'attribute' => 'target_id', // The attribute for each
-   *                                  // field value to use.
-   *                                  // Defaults to target_id
-   *      'multiple' => 'or', // or 'and'. Defaults to 'or'
-   *      'all_value' => 'all', // Defaults to 'all'
-   *    ],
-   *  ];
-   *
-   * @return array
-   *   The argument mappings
+   * {@inheritdoc}
    */
-  // protected function argumentMappings() {
-  //   return [
-  //     'field_drupal_term_filter' => [
-  //       'index' => 0,
-  //       'view' => 'events_collection',
-  //       'multiple' => 'or',
-  //     ],
-  //   ];
-  // }
+  protected function prepareView($view, $build, $entity, $display, $view_mode) {
 
-  // protected function prepareView($view, $build, $entity, $display, $view_mode) {
-  //   $show_pagination = ($entity->get('field_show_pagination')->value) ? $entity->get('field_show_pagination')->value : FALSE;
-  //   $total_posts = ($entity->get('field_total_posts')->value) ? $entity->get('field_total_posts')->value : 4;
-  //   $view->setItemsPerPage($total_posts);
-  //   if($show_pagination) {
-  //     $view->display_handler->options['pager']['type'] = 'full';
-  //   }
-  // 
-  //   return $view;
-  // }
+    if ($entity->hasField('field_display_option') && !$entity->get('field_display_option')->isEmpty()) {
+
+      // Set view display based on the assembly Display Option field.
+      $display_option = $entity->get('field_display_option')->value;
+      $view->setDisplay($display_option);
+
+      // Get sessions for a single event.
+      if ($display_option == 'single_event_sessions') {
+
+        // Get sessions from reference field on assembly.
+        if ($entity->hasField('field_single_event_reference') && !$entity->get('field_single_event_reference')->isEmpty()) {
+          if ($single_event_tid = $entity->get('field_single_event_reference')->getValue()[0]['target_id']) {
+            $view->setArguments([$single_event_tid]);
+          }
+        }
+        // Get sessions from the event node the assembly is used on.
+        else {
+          // @TODO Get event node id from assembly.
+        }
+      }
+    }
+
+    return $view;
+  }
 
 }
